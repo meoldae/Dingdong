34184ab74c1083514f0b128707581a6f978a4c17
Junyoung <wnsdud12365@naver.com>
2023-10-21 20:34:48 +0900

    FIX: Response Status Code String으로 변경

--- backend/src/main/java/com/ssafy/dingdong/global/response/ResponseService.java
+++ backend/src/main/java/com/ssafy/dingdong/global/response/ResponseService.java
@@ -8,7 +8,7 @@ public class ResponseService {
 	/* 정상 응답, 데이터 X */
 	public CommonResponse successResponse(ResponseStatus responseStatus){
 		CommonResponse successResponse = new CommonResponse();
-		successResponse.setCode(responseStatus.getCode());
+		successResponse.setCode(responseStatus.getCode().getCode());
 		successResponse.setMessage(responseStatus.getMessage());
 		return successResponse;
 	}
@@ -16,7 +16,7 @@ public class ResponseService {
 	/* 정상 응답 - 데이터 포함 */
 	public <T> DataResponse<?> successDataResponse(ResponseStatus responseStatus, T data){
 		DataResponse<T> dataResponse = new DataResponse<>();
-		dataResponse.setCode(responseStatus.getCode());
+		dataResponse.setCode(responseStatus.getCode().getCode());
 		dataResponse.setMessage(responseStatus.getMessage());
 		dataResponse.setData(data);
 		return dataResponse;
@@ -25,7 +25,7 @@ public class ResponseService {
 	/* 비정상 응답 - 데이터 미포함 */
 	public CommonResponse failureCommonResponse(ResponseStatus responseStatus) {
 		CommonResponse response = new CommonResponse();
-		response.setCode(responseStatus.getCode());
+		response.setCode(responseStatus.getCode().getCode());
 		response.setMessage(responseStatus.getMessage());
 		return response;
 	}
@@ -33,7 +33,7 @@ public class ResponseService {
 	/* 비정상 응답 - 데이터 포함 */
 	public <T> DataResponse<?> failureDataResponse(ResponseStatus responseStatus, T data) {
 		DataResponse<T> dataResponse = new DataResponse<>();
-		dataResponse.setCode(responseStatus.getCode());
+		dataResponse.setCode(responseStatus.getCode().getCode());
 		dataResponse.setMessage(responseStatus.getMessage());
 		dataResponse.setData(data);
 		return dataResponse;
