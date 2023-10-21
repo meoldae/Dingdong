9bf7636fb33708cf7ed2503f8c9ac3e1e3510275
Junyoung <wnsdud12365@naver.com>
2023-10-21 19:14:49 +0900

    FEAT: Member 엔티티 추가

--- backend/src/main/java/com/ssafy/dingdong/domain/member/entity/Member.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/member/entity/Member.java
@@ -1,17 +1,32 @@
 package com.ssafy.dingdong.domain.member.entity;
 
+import lombok.Getter;
 import lombok.extern.log4j.Log4j;
 import org.hibernate.annotations.Type;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Log4j
+@Getter
 public class Member {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Type(type="org.hibernate.type.UUIDCharType")
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
-    private UUID id;
+    private UUID memberId;
+    private String provider;
+    private String username;
+
+    @Length(min = 1, max = 8)
+    private String nickname;
+    private String email;
+    private LocalDateTime createTime;
+    private LocalDateTime exitTime;
+    private Long characterId;
 }
