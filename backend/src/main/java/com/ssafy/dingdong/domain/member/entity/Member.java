e95d87e3dacb1eb02e6b66118a9e72a58b52c8ba
Junyoung <wnsdud12365@naver.com>
2023-10-21 22:37:38 +0900

    FEAT: Member Entity 구성 및 회원가입 메서드 추가

--- backend/src/main/java/com/ssafy/dingdong/domain/member/entity/Member.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/member/entity/Member.java
@@ -1,7 +1,12 @@
 package com.ssafy.dingdong.domain.member.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j;
+
+import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.Length;
 
@@ -13,12 +18,14 @@ import java.util.UUID;
 @Entity
 @Log4j
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Type(type="org.hibernate.type.UUIDCharType")
-    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Type(type="uuid-char")
+    @Column(length = 36, updatable = false, nullable = false)
     private UUID memberId;
     private String provider;
     private String username;
@@ -29,4 +36,16 @@ public class Member {
     private LocalDateTime createTime;
     private LocalDateTime exitTime;
     private Long characterId;
+
+    @Builder
+    public Member(String provider, String email, LocalDateTime createTime){
+        this.provider = provider;
+        this.email = email;
+        this.createTime = createTime;
+    }
+
+    public void signUp(String nickname, Long characterId){
+        this.nickname = nickname;
+        this.characterId = characterId;
+    }
 }
