c21c387d9bb0da685a176b140e9e487e56132544
Junyoung <wnsdud12365@naver.com>
2023-10-21 22:38:47 +0900

    FEAT: 회원가입 서비스 구현 ( 미완성 )

--- backend/src/main/java/com/ssafy/dingdong/domain/member/service/MemberService.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/member/service/MemberService.java
@@ -1,4 +1,9 @@
 package com.ssafy.dingdong.domain.member.service;
 
+import com.ssafy.dingdong.domain.member.dto.request.MemberSignUpDto;
+import com.ssafy.dingdong.domain.member.dto.response.MemberMainDto;
+
 public interface MemberService {
+
+	MemberMainDto createMember(MemberSignUpDto memberLoginDto);
 }
