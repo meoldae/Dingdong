c47e341f869a514e877501dda7b7b3a61b21e3e1
Junyoung <wnsdud12365@naver.com>
2023-10-21 22:36:09 +0900

    FEAT: 회원가입( 닉네임 설정, 캐릭터 선택) 메서드 추가

--- backend/src/main/java/com/ssafy/dingdong/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/member/controller/MemberController.java
@@ -1,11 +1,32 @@
 package com.ssafy.dingdong.domain.member.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.dingdong.domain.member.dto.request.MemberSignUpDto;
+import com.ssafy.dingdong.domain.member.dto.response.MemberMainDto;
+import com.ssafy.dingdong.domain.member.service.MemberService;
+import com.ssafy.dingdong.global.response.DataResponse;
+import com.ssafy.dingdong.global.response.ResponseService;
+import com.ssafy.dingdong.global.response.ResponseStatus;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
 @RestController
 @RequiredArgsConstructor
-public class MemberController {
+public class MemberController implements MemberSwaggerController {
+
+	private final ResponseService responseService;
+	private final MemberService memberService;
+
+	@Override
+	public DataResponse createMember(@Validated @RequestBody MemberSignUpDto memberLoginDto, HttpServletRequest response) {
+		MemberMainDto member = memberService.createMember(memberLoginDto);
+		return responseService.successDataResponse(ResponseStatus.RESPONSE_SUCCESS, member);
+	}
 }
