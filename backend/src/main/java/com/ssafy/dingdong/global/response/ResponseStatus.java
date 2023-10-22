8204e607ac6a05069be83c144f94610b798488af
Junyoung <wnsdud12365@naver.com>
2023-10-22 16:34:49 +0900

    FEAT: 세션 활성화/비활성화 상태 추가

--- backend/src/main/java/com/ssafy/dingdong/global/response/ResponseStatus.java
+++ backend/src/main/java/com/ssafy/dingdong/global/response/ResponseStatus.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ResponseStatus {
     /* 공통 */
-    RESPONSE_SUCCESS(ResponseStatusCode.SUCCESS, "요청에 성공했습니다.");
+    RESPONSE_SUCCESS(ResponseStatusCode.SUCCESS, "요청에 성공했습니다."),
+    SESSION_CREATED(ResponseStatusCode.SUCCESS, "접속되었습니다."),
+    SESSION_DELETED(ResponseStatusCode.SUCCESS, "접속 종료하였습니다.");
 
     private final ResponseStatusCode code;
     private final String message;
