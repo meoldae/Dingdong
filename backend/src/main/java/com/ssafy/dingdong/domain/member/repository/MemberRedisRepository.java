7f31b25d62ffec6002ca8694f382b386d507bbed
Junyoung <wnsdud12365@naver.com>
2023-10-23 21:47:25 +0900

    REFACTOR: RedisRepository to RedisTemplate으로 변경

--- backend/src/main/java/com/ssafy/dingdong/domain/member/repository/MemberRedisRepository.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/member/repository/MemberRedisRepository.java
@@ -1,12 +1,17 @@
-// package com.ssafy.dingdong.domain.member.repository;
-//
-// import org.springframework.data.repository.CrudRepository;
-// import org.springframework.stereotype.Repository;
-//
-// import com.ssafy.dingdong.domain.member.entity.RefreshToken;
-//
-// @Repository
-// public interface MemberRedisRepository extends CrudRepository<RefreshToken, String> {
-//
-// 	void saveToken(String memberId, String accessToken, String refreshToken);
-// }
+package com.ssafy.dingdong.domain.member.repository;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRedisRepository {
+
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	public void saveToken(String memberId, String accessToken, String refreshToken) {
+
+	}
+}
