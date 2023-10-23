3964cf7d5ff20dd956b53fca778d13fd48cdfade
Kang Dong Pyo <76652908+97Kzone@users.noreply.github.com>
2023-10-23 19:35:13 +0900

    FIX: 소셜 로그인 api 삭제

--- frontend/src/pages/User/LoginPage.jsx
+++ frontend/src/pages/User/LoginPage.jsx
@@ -1,22 +1,20 @@
 // 스타일
-import style from "./LoginPage.css"
+import style from "./LoginPage.css";
 
 // 통신
-import axios from "axios"
+import axios from "axios";
 
 const LoginPage = () => {
   const loginHandler = () => {
-    console.log(import.meta.env.VITE_SERVER_URL)
-    window.location.href = `https://${
-      import.meta.env.VITE_SERVER_URL
-    }/api/oauth2/authorization/kakao`
-  }
+    console.log(import.meta.env.VITE_SERVER_URL);
+    window.location.href = `https://${import.meta.env.VITE_SERVER_URL}/oauth2/authorization/kakao`;
+  };
 
   return (
     <div>
       <button onClick={loginHandler}>로그인</button>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
