5f9cef6ff79781546781554314370af1ecebd400
Junyoung <wnsdud12365@naver.com>
2023-10-23 09:42:08 +0900

    STYLE: Controller 어노테이션 위치 변경

--- backend/src/main/java/com/ssafy/dingdong/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/member/controller/MemberController.java
@@ -4,8 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.dingdong.domain.member.dto.request.MemberSignUpDto;
@@ -22,36 +26,42 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController implements MemberSwaggerController {
 
 	private final ResponseService responseService;
 	private final MemberService memberService;
 
 	@Override
+	@PostMapping("/signup")
 	public DataResponse createMember(@Validated @RequestBody MemberSignUpDto memberSignUpDto, HttpServletRequest response) {
 		MemberMainDto member = memberService.createMember(memberSignUpDto);
 		return responseService.successDataResponse(ResponseStatus.RESPONSE_SUCCESS, member);
 	}
 
 	@Override
+	@DeleteMapping("/logout")
 	public CommonResponse logout(Authentication authentication) {
 		memberService.logout(authentication.getName());
 		return responseService.successResponse(ResponseStatus.RESPONSE_SUCCESS);
 	}
 
 	@Override
+	@GetMapping("/{memberId}")
 	public DataResponse getMember(@PathVariable String memberId) {
 		MemberMainDto member = memberService.getMemberById(memberId);
 		return responseService.successDataResponse(ResponseStatus.RESPONSE_SUCCESS, member);
 	}
 
 	@Override
+	@GetMapping("/connect")
 	public CommonResponse createSession(Authentication authentication) {
 		memberService.createSession(authentication.getName());
 		return responseService.successResponse(ResponseStatus.SESSION_CREATED);
 	}
 
 	@Override
+	@DeleteMapping("/disconnect")
 	public CommonResponse deleteSession(Authentication authentication) {
 		memberService.deleteSession(authentication.getName());
 		return responseService.successResponse(ResponseStatus.SESSION_DELETED);
