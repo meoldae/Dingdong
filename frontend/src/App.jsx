570cc02a99c6aa0f2d4d0bbd01cb980caaa34417
김준형 <wnsgud6232@naver.com>
2023-10-23 14:57:41 +0900

    STYLE: max-width 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,18 @@ import ReactDOM from "react-dom/client"
 import { RecoilRoot } from "recoil"
 import AppRouter from "./router/AppRouter.jsx"
 import "./fonts/Fonts.css"
+import "./App.css"
+
+const AppWrapper = () => {
+  return (
+    <div className="appWrapper">
+      <AppRouter />
+    </div>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RecoilRoot>
-    <AppRouter />
+    <AppWrapper />
   </RecoilRoot>
 )
