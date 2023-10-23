45c54a6537c3179ec6e983782ede38474c879f0a
김준형 <wnsgud6232@naver.com>
2023-10-24 02:33:43 +0900

    FEAT: 이웃신청 알림 모달 작성

--- frontend/src/components/Modal/Neighbor/NeighborModal.jsx
+++ frontend/src/components/Modal/Neighbor/NeighborModal.jsx
@@ -1,7 +1,31 @@
-import style from "./NeighborModal.css"
+import styles from "./NeighborModal.module.css"
 
-const NeighborModal = () => {
-  return <div>이웃 모달</div>
+const NeighborModal = ({ content, okClick, cancelClick }) => {
+  return (
+    <div className={styles.Container}>
+      <div className={styles.ContentContainer}>
+        {content}으로 부터 이웃신청이 왔습니다.
+      </div>
+      <div className={styles.HorizontalLine} />
+      <div className={styles.VerticalLine} />
+      <div className={styles.ButtonContainer}>
+        <div
+          className={styles.Button}
+          style={{ color: "#049463" }}
+          onClick={okClick}
+        >
+          수락
+        </div>
+        <div
+          className={styles.Button}
+          style={{ color: "#2C2C2C" }}
+          onClick={cancelClick}
+        >
+          거절
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NeighborModal
