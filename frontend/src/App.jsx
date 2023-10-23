d6a1e70ddf25eb218a228c219a69f492be1cfc55
김준형 <wnsgud6232@naver.com>
2023-10-23 14:14:03 +0900

    FEAT: 폰트 적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client"
 import { RecoilRoot } from "recoil"
 import AppRouter from "./router/AppRouter.jsx"
+import "./fonts/Fonts.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RecoilRoot>
