032ab68f19b37c006276876f98f39dab047b32c8
KyongBeom <dragontig98@naver.com>
2023-10-23 10:29:40 +0900

    MERGE: 마이룸 프로젝트 추가

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rf1h54cqug"
+    "revision": "0.b3oipa5fri"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
-
+import { Canvas } from "@react-three/fiber"
+import Experience from "./components/Experience";
+import './App.css';
 function App() {
   return (
     <>
-
+      <Canvas shadows camera={{ position: [10, 10, 20], fov: 60 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Experience />
+      </Canvas>
     </>
   );
 }
