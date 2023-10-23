265ce5f6b960835f012b4d550f3ca8a73ee9fe4c
Junyoung <wnsdud12365@naver.com>
2023-10-23 21:59:43 +0900

    FEAT: Login 시 AccessToken, RefreshToken 저장

--- backend/src/main/java/com/ssafy/dingdong/domain/member/repository/MemberRedisRepository.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/member/repository/MemberRedisRepository.java
@@ -1,6 +1,9 @@
 package com.ssafy.dingdong.domain.member.repository;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
 import lombok.RequiredArgsConstructor;
@@ -9,9 +12,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberRedisRepository {
 
+	private final String ACCESS_TOKEN = "accessToken";
+	private final String REFRESH_TOKEN = "refreshToken";
+
 	private final RedisTemplate<String, Object> redisTemplate;
+	private ValueOperations<String, Object> valueOperations;
+
+	@PostConstruct
+	public void init(){
+		valueOperations = redisTemplate.opsForValue();
+	}
 
 	public void saveToken(String memberId, String accessToken, String refreshToken) {
-
+		valueOperations.set(ACCESS_TOKEN + memberId, accessToken);
+		valueOperations.set(REFRESH_TOKEN + memberId, refreshToken);
 	}
 }
