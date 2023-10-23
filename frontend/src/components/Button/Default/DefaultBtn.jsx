4acf2bc40d7ec6bd97d57e4cb190d274c51d8a2f
김준형 <wnsgud6232@naver.com>
2023-10-23 22:23:01 +0900

    FEAT: 기본 버튼 완성

--- frontend/src/components/Button/Default/DefaultBtn.jsx
+++ frontend/src/components/Button/Default/DefaultBtn.jsx
@@ -1,9 +1,21 @@
 import styles from "./DefaultBtn.module.css"
 
-const DefaultBtn = ({ btnName, onClick }) => {
+const DefaultBtn = ({ btnName, onClick, color }) => {
+  const checkColor = () => {
+    if (color === "#049463") {
+      return { backgroundColor: color, color: "#FFFFFF" }
+    } else {
+      return { backgroundColor: "#F2CBE4", color: "#2C2C2C" }
+    }
+  }
+
   return (
-    <div className={styles.ButtonContainer} onClick={onClick}>
-      <p>{btnName}</p>
+    <div
+      className={styles.ButtonContainer}
+      onClick={onClick}
+      style={checkColor()}
+    >
+      <p className={styles.ButtonContent}>{btnName}</p>
     </div>
   )
 }
