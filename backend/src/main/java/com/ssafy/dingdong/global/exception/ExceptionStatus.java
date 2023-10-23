840fbf5282e7c750fb6476d2e052809883845298
Junyoung <wnsdud12365@naver.com>
2023-10-23 10:17:59 +0900

    FEAT: Token Refresh 관련 상태 추가

--- backend/src/main/java/com/ssafy/dingdong/global/exception/ExceptionStatus.java
+++ backend/src/main/java/com/ssafy/dingdong/global/exception/ExceptionStatus.java
@@ -14,7 +14,9 @@ public enum ExceptionStatus {
 	LETTER_FOUND_EXCEPTION(ResponseStatusCode.ERROR, "편지 목록 조회 중 문제가 발생했습니다"),
 	LETTER_NOT_FOUND(ResponseStatusCode.ERROR, "편지를 찾을 수 없습니다."),
 	MEMBER_NOT_FOUND(ResponseStatusCode.ERROR, "회원을 찾을 수 없습니다."),
-	TOKEN_EXPIRED(ResponseStatusCode.ERROR, "토큰이 만료되었습니다.");
+	TOKEN_EXPIRED(ResponseStatusCode.ERROR, "토큰이 만료되었습니다."),
+	REFRESH_TOKEN_NOT_FOUND_IN_COOKIE(ResponseStatusCode.ERROR, "리프레시 토큰이 없습니다."),
+	REFRESH_TOKEN_EXPIRED(ResponseStatusCode.ERROR, "리프레시 토큰이 만료되었습니다.");
 
 	private final ResponseStatusCode code;
 	private final String message;
