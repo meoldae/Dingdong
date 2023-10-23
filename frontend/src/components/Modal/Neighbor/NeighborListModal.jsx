0c9441ee5ef35249d78efe8cadd5204c7dc23e27
김준형 <wnsgud6232@naver.com>
2023-10-24 03:00:29 +0900

    FEAT: 이웃리스트 모달 작성

--- frontend/src/components/Modal/Neighbor/NeighborListModal.jsx
+++ frontend/src/components/Modal/Neighbor/NeighborListModal.jsx
@@ -1,7 +1,31 @@
 import styles from "./NeighborListModal.module.css"
 
-const NeighborListModal = () => {
-  return <div></div>
+const NeighborListModal = ({ profile, nickname }) => {
+  return (
+    <div className={styles.Container}>
+      <div className={styles.ProfileContainer}>
+        <img src={profile} />
+      </div>
+      <div className={styles.InfoContainer}>
+        <div
+          style={{
+            fontFamily: "Pretendard-SemiBold",
+            fontSize: "20px",
+            marginLeft: "15px",
+          }}
+        >
+          {nickname}
+        </div>
+        <div>
+          <img src={"assets/icons/home.svg"} style={{ marginRight: "10px" }} />
+          <img
+            src={"assets/icons/removeNeighbor.svg"}
+            style={{ marginRight: "15px" }}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NeighborListModal
