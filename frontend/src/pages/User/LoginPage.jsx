9e7bc467388d7caaf82829eeb4829b19198e2712
김준형 <wnsgud6232@naver.com>
2023-10-23 12:25:27 +0900

    Feat: 카카오로그인 기능 구현

--- frontend/src/pages/User/LoginPage.jsx
+++ frontend/src/pages/User/LoginPage.jsx
@@ -1,7 +1,20 @@
+// 스타일
 import style from "./LoginPage.css"
 
+// 통신
+import axios from "axios"
+
 const LoginPage = () => {
-  return <>로그인 페이지</>
+  const loginHandler = () => {
+    window.location.href =
+      "https://k9b203.p.ssafy.io/api/oauth2/authorization/kakao"
+  }
+
+  return (
+    <div>
+      <button onClick={loginHandler}>로그인</button>
+    </div>
+  )
 }
 
 export default LoginPage
