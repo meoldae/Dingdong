d36b6ad042df1e711f9025ca414c4b1f400e21f8
김준형 <wnsgud6232@naver.com>
2023-10-23 21:28:18 +0900

    STYLE: 룸 버튼에 이미지 추가

--- frontend/src/components/Button/Room/RoomBtn.jsx
+++ frontend/src/components/Button/Room/RoomBtn.jsx
@@ -1,7 +1,11 @@
 import styles from "./RoomBtn.module.css"
 
-const RoomBtn = () => {
-  return <div className={styles.circle}></div>
+const RoomBtn = ({ img }) => {
+  return (
+    <div className={styles.circle}>
+      <img src={`assets/icons/${img}.svg`} />
+    </div>
+  )
 }
 
 export default RoomBtn
