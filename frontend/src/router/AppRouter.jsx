3b9232ed21714833d79cbb150724d3d63bf9aad8
김준형 <wnsgud6232@naver.com>
2023-10-23 10:59:19 +0900

    Feat: 라우터 설정

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,3 +1,28 @@
-const AppRouter = () => {}
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+// 라우터
+import AppMain from "../pages/Main/MainPage"
+import AppRoom from "../pages/Room/RoomPage"
+import AppNotFound from "../pages/NotFound/NotFound"
+import AppLogin from "../pages/User/LoginPage"
+import AppSignUp from "../pages/User/SignUpPage"
+import AppUserSetting from "../pages/User/UserSettingPage"
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/" element={<AppMain />} />
+          <Route path="/room" element={<AppRoom />} />
+          <Route path="/login" element={<AppLogin />} />
+          <Route path="/signup" element={<AppSignUp />} />
+          <Route path="/usersetting" element={<AppUserSetting />} />
+        </Route>
+        <Route path="/*" element={<AppNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default AppRouter
