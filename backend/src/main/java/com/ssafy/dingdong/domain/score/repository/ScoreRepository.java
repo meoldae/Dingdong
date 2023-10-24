17bcc9273ec64ae401423fb0abc71baf828e1369
Dayoung1014 <58163364+Dayoung1014@users.noreply.github.com>
2023-10-24 20:52:38 +0900

    FEAT: 스코어 보드 조회

--- backend/src/main/java/com/ssafy/dingdong/domain/score/repository/ScoreRepository.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/score/repository/ScoreRepository.java
@@ -6,11 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface ScoreRepository extends JpaRepository<Score, Long> {
 
     @Query("SELECT s FROM Score s WHERE s.type = ?1 ORDER BY s.recordTime DESC")
     List<Score> findLatestByType(ScoreType type, Pageable pageable);
+
+    @Query("SELECT MAX(s.recordTime) FROM Score s")
+    Optional<LocalDateTime> findLatestRecordTime();
 
 }
