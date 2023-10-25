e79cbf2ca2fa3da3b214670cdf07fb8abb26dfd7
김장호 <rlawkdgh0406@naver.com>
2023-10-26 00:11:10 +0900

    CHORE: 임시 맵 저장

--- frontend/src/components/Default/TempMap.jsx
+++ frontend/src/components/Default/TempMap.jsx
@@ -1,33 +1,33 @@
 // Map.jsx
-import React, { useEffect, useRef } from "react"
-import { extend, useThree, useLoader } from "@react-three/fiber"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import React, { useEffect, useRef } from "react";
+import { extend, useThree, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 // GLTFLoader를 react-three-fiber의 hook에서 사용 가능하도록 확장
-extend({ GLTFLoader })
+extend({ GLTFLoader });
 
 const TempMap = () => {
-  const { scene } = useThree()
-  const gltf = useLoader(GLTFLoader, "assets/models/map.glb")
+  const { scene } = useThree();
+  const gltf = useLoader(GLTFLoader, "assets/models/floor.glb");
 
-  const mapRef = useRef(null)
+  const mapRef = useRef(null);
 
   useEffect(() => {
     if (gltf && gltf.scene) {
-      gltf.scene.position.set(0, 0, 0) // 회전값 설정
-      gltf.scene.name = "floor"
-      scene.add(gltf.scene)
-      mapRef.current = gltf.scene
+      gltf.scene.position.set(0, 0, 0); // 회전값 설정
+      gltf.scene.name = "floor";
+      scene.add(gltf.scene);
+      mapRef.current = gltf.scene;
     }
 
     return () => {
       if (gltf && gltf.scene) {
-        scene.remove(gltf.scene)
+        scene.remove(gltf.scene);
       }
-    }
-  }, [gltf, scene])
+    };
+  }, [gltf, scene]);
 
-  return null
-}
+  return null;
+};
 
-export default TempMap
+export default TempMap;
