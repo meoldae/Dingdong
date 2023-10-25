44d378c116c160e3525de0262f0ab19411d2dbc4
Junyoung <wnsdud12365@naver.com>
2023-10-25 20:07:41 +0900

    REFACTOR: 가구 에셋 관리 전략 변경에 따른 엔티ã티 수정

--- backend/src/main/java/com/ssafy/dingdong/domain/room/entity/Room.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/room/entity/Room.java
@@ -1,24 +1,33 @@
 package com.ssafy.dingdong.domain.room.entity;
 
+import java.util.List;
+
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 @Getter
 @Builder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long roomId;
     private String memberId;
-    private String glbUrl;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "roomId")
+    private List<Furniture> furnitureList;
 }
