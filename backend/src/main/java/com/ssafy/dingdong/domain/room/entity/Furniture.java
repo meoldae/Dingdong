5620871181ec15a931db5e7fb552aa6c9375f3db
Junyoung <wnsdud12365@naver.com>
2023-10-25 20:07:02 +0900

    FEAT: 가구 엔티티 생성

---
+++ backend/src/main/java/com/ssafy/dingdong/domain/room/entity/Furniture.java
@@ -0,0 +1,24 @@
+package com.ssafy.dingdong.domain.room.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Furniture {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long furnitureId;
+	private Long roomId;
+	private String assetId;
+	private Float xPos;
+	private Float yPos;
+	private Float zPos;
+	private Short rotation;
+
+}
