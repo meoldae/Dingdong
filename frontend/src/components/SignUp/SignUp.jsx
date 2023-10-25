ccd9880d8dd4c0801910e07c5e4a970e91d9ca26
Kang Dong Pyo <76652908+97Kzone@users.noreply.github.com>
2023-10-25 20:25:00 +0900

    FIX: 회원가입 후 처리 수정

--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -1,104 +1,124 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import style from "./SignUp.module.css"
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import style from "./SignUp.module.css";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { CreateUser, GetAvatarList } from "@/api/User";
 
 const SignUp = () => {
-    const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
-    const [charactersData, setCharactersData] =  useState([]);
-    const [avatarId, setAvatar] = useState(null);
-    const [nickname, setNickname] = useState("");
+  const [charactersData, setCharactersData] = useState([]);
+  const [avatarId, setAvatar] = useState(null);
+  const [nickname, setNickname] = useState("");
 
-    useEffect(() => {
-        GetAvatarList((response) => {
-            const avatarList = response.data.data.avatarList;
-            const formattedData = Object.keys(avatarList).map(key => ({ id: parseInt(key), glb: avatarList[key] }));
-            
-            setCharactersData(formattedData);
-            setAvatar(formattedData[0].id);
-        }, (error) => {
-            console.error("Error fetching avatars:", error);
-        });
-    }, []);
+  useEffect(() => {
+    GetAvatarList(
+      (response) => {
+        const avatarList = response.data.data.avatarList;
+        const formattedData = Object.keys(avatarList).map((key) => ({
+          id: parseInt(key),
+          glb: avatarList[key],
+        }));
 
-    const charactersImages = charactersData.map(charData => charData.glb);
-    const memberId = new URLSearchParams(window.location.search).get("memberId");
+        setCharactersData(formattedData);
+        setAvatar(formattedData[0].id);
+      },
+      (error) => {
+        console.error("Error fetching avatars:", error);
+      }
+    );
+  }, []);
 
-    const NextArrow = ({ onClick }) => {
-        return (
-            <div className={style.arrow + " " + style.next} onClick={onClick}>
-                    ➡️
-                </div>
-            );
-        };
+  const charactersImages = charactersData.map((charData) => charData.glb);
+  const memberId = new URLSearchParams(window.location.search).get("memberId");
 
-    const PrevArrow = ({ onClick }) => {
-        return (
-            <div className={style.arrow + " " + style.prev} onClick={onClick}>
-                ⬅️
-            </div>
-        );
-    };
-    const settings = {
-        centerMode: true,
-        infinite: true,
-        centerPadding: "0",
-        slidesToShow: 1,
-        speed: 500,
-        arrows: true,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        afterChange: current => handleSlideChange(current)
-    };
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className={style.arrow + " " + style.next} onClick={onClick}>
+        ➡️
+      </div>
+    );
+  };
 
-    const handleSlideChange = (index) => {
-        setAvatar(charactersData[index].id);
-    };
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className={style.arrow + " " + style.prev} onClick={onClick}>
+        ⬅️
+      </div>
+    );
+  };
+  const settings = {
+    centerMode: true,
+    infinite: true,
+    centerPadding: "0",
+    slidesToShow: 1,
+    speed: 500,
+    arrows: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    afterChange: (current) => handleSlideChange(current),
+  };
 
-    async function doSignUp() {
-        if (!avatarId || !nickname) {
-            window.alert("캐릭터와 닉네임을 모두 선택해주세요");
-            return;
-        }
-        
-        const param = { memberId, avatarId, nickname };
+  const handleSlideChange = (index) => {
+    setAvatar(charactersData[index].id);
+  };
 
-        await CreateUser(param, (response) => {
-            navigate("/");
-        }, (error) => {console.log(error)});
+  async function doSignUp() {
+    if (!avatarId || !nickname) {
+      window.alert("캐릭터와 닉네임을 모두 선택해주세요");
+      return;
     }
 
-    return (
+    const param = { memberId, avatarId, nickname };
+
+    await CreateUser(param,(response) => {
+      const token = response.data.data.accessToken;
+      console.log(token);
+        navigate(`/oauth2/redirect?token=${token}`);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  return (
+    <div>
+      <div>
+        <span style={{ color: "#049463" }}>프로필 </span>
+        <span style={{ color: "#2C2C2C" }}>선택</span>
+
+        <Slider {...settings}>
+          {charactersImages.map((charImg, idx) => (
+            <img
+              key={idx}
+              src={charImg}
+              alt=""
+              className={style.characterImage}
+              onClick={() => setAvatar(charImg)}
+            />
+          ))}
+        </Slider>
+
         <div>
-            <div>
-                <span style={{ color: "#049463" }}>프로필 </span>
-                <span style={{ color: "#2C2C2C" }}>선택</span>
-
-                <Slider {...settings}>
-                    {charactersImages.map((charImg, idx) => (
-                        <img key={idx} src={charImg} alt="" className={style.characterImage} onClick={() => setAvatar(charImg)} />
-                    ))}
-                </Slider>
-
-                <div>
-                    <div>닉네임: </div>
-                    <div>
-                        <input type="text" 
-                               value={nickname} 
-                               onChange={(e) => setNickname(e.target.value)} 
-                               placeholder="닉네임을 입력해주세요" />
-                    </div>
-                </div>
-
-                <div>
-                    <button onClick={doSignUp}>회원가입</button>
-                </div>
-            </div>
+          <div>닉네임: </div>
+          <div>
+            <input
+              type="text"
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+              placeholder="닉네임을 입력해주세요"
+            />
+          </div>
         </div>
-    )
-}
+
+        <div>
+          <button onClick={doSignUp}>회원가입</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default SignUp;
