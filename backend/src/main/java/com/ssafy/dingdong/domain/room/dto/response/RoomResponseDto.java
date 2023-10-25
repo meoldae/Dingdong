748bd8cb09ed4e195ea5f86ec3a7d104ed9f2f5d
Junyoung <wnsdud12365@naver.com>
2023-10-25 22:02:47 +0900

    REFACTOR: Furniture -> RoomFurniture로 이름 변경

--- backend/src/main/java/com/ssafy/dingdong/domain/room/dto/response/RoomResponseDto.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/room/dto/response/RoomResponseDto.java
@@ -2,17 +2,17 @@ package com.ssafy.dingdong.domain.room.dto.response;
 
 import java.util.List;
 
-import com.ssafy.dingdong.domain.room.entity.Furniture;
+import com.ssafy.dingdong.domain.room.entity.RoomFurniture;
 
 import lombok.Builder;
 
 public record RoomResponseDto(
 	Long roomId,
-	List<Furniture> furnitureList
+	List<RoomFurniture> roomFurnitureList
 ) {
 	@Builder
-	public RoomResponseDto(Long roomId, List<Furniture> furnitureList) {
+	public RoomResponseDto(Long roomId, List<RoomFurniture> roomFurnitureList) {
 		this.roomId = roomId;
-		this.furnitureList = furnitureList;
+		this.roomFurnitureList = roomFurnitureList;
 	}
 }
