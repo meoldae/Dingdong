870ec3d1f1ad2562114ebe42de719faf53ad1282
Junyoung <wnsdud12365@naver.com>
2023-10-26 21:36:59 +0900

    FEAT: 스코어를 위한 DTO 추가

---
+++ backend/src/main/java/com/ssafy/dingdong/domain/room/dto/response/RoomScoreDto.java
@@ -0,0 +1,21 @@
+package com.ssafy.dingdong.domain.room.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class RoomScoreDto{
+	private Long roomId;
+	private String nickname;
+	private Long heartCount;
+
+	@Builder
+	public RoomScoreDto(Long roomId, Long heartCount){
+		this.roomId = roomId;
+		this.heartCount = heartCount;
+	}
+
+	public void setNickname(String nickname){
+		this.nickname = nickname;
+	}
+}
