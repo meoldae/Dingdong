536d5cc085faec3ee42160685d76904686d231e7
Junyoung <wnsdud12365@naver.com>
2023-10-26 09:30:06 +0900

    FEAT: 가구 리스트 반환 Dto 생성

--- backend/src/main/java/com/ssafy/dingdong/domain/room/dto/response/FurnitureSummaryDto.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/room/dto/response/FurnitureSummaryDto.java
@@ -1,10 +1,13 @@
 package com.ssafy.dingdong.domain.room.dto.response;
 
+import lombok.Builder;
+
 public record FurnitureSummaryDto(
 	String furnitureId,
 	Long categoryId,
 	String thumbnail
 ) {
+	@Builder
 	public FurnitureSummaryDto(String furnitureId, Long categoryId, String thumbnail){
 		this.furnitureId = furnitureId;
 		this.categoryId = categoryId;
