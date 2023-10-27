ace15eda272c1b850b88f1b755ba99181e7702be
김준형 <wnsgud6232@naver.com>
2023-10-27 22:25:04 +0900

    CHORE: 코드 프리티어 적용

--- frontend/src/components/Footer/OtherFooter.jsx
+++ frontend/src/components/Footer/OtherFooter.jsx
@@ -1,34 +1,35 @@
-import { useEffect, useState } from 'react';
-import PostCardBox from '../Modal/Post/PostCardBox';
-import RoomBtn from "../Button/Room/RoomBtn";
-import SendLetter from '../Modal/Post/SendLetter';
-import style from "./Footer.module.css";
+import { useEffect, useState } from "react"
+import PostCardBox from "../Modal/Post/PostCardBox"
+import RoomBtn from "../Button/Room/RoomBtn"
+import SendLetter from "../Modal/Post/SendLetter"
+import style from "./Footer.module.css"
 
 const OtherFooter = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isSendLetterModalVisible, setIsSendLetterModalVisible] = useState(false);
-  const [selectedPostCard, setSelectedPostCard] = useState(null);
-  
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [isSendLetterModalVisible, setIsSendLetterModalVisible] =
+    useState(false)
+  const [selectedPostCard, setSelectedPostCard] = useState(null)
+
   const openModal = () => {
-    setIsModalVisible(true);
+    setIsModalVisible(true)
   }
 
   const closeModal = () => {
-    setIsModalVisible(false);
+    setIsModalVisible(false)
   }
 
   const openSendLetterModal = () => {
-    setIsSendLetterModalVisible(true);
+    setIsSendLetterModalVisible(true)
   }
 
   const closeSendLetterModal = () => {
-    setIsSendLetterModalVisible(false);
+    setIsSendLetterModalVisible(false)
   }
 
   const handleSelectButtonClick = (selectedCard) => {
-    setSelectedPostCard(selectedCard);
-    closeModal(); 
-    openSendLetterModal();
+    setSelectedPostCard(selectedCard)
+    closeModal()
+    openSendLetterModal()
   }
 
   return (
@@ -43,13 +44,20 @@ const OtherFooter = () => {
           <RoomBtn img={"worldMap"} />
         </div>
         <div className={style.background}>
-          <RoomBtn img={"post"} onClick={openModal}/>
+          <RoomBtn img={"post"} onClick={openModal} />
         </div>
       </div>
-      {isModalVisible && <PostCardBox cancelClick={closeModal} onSelectButtonClick={handleSelectButtonClick}/>}
-      {isSendLetterModalVisible && <SendLetter onClose={closeSendLetterModal} card={selectedPostCard}/>}
+      {isModalVisible && (
+        <PostCardBox
+          cancelClick={closeModal}
+          onSelectButtonClick={handleSelectButtonClick}
+        />
+      )}
+      {isSendLetterModalVisible && (
+        <SendLetter onClose={closeSendLetterModal} card={selectedPostCard} />
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default OtherFooter;
+export default OtherFooter
