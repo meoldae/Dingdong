91257352fcb8a318d02140bd836533b20eba0d3d
김준형 <wnsgud6232@naver.com>
2023-10-27 20:11:09 +0900

    CHORE: 코드에 프리티어 적용

--- frontend/src/components/Modal/Post/SendLetter.jsx
+++ frontend/src/components/Modal/Post/SendLetter.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
+import { useState } from "react"
 import DefaultBtn from "../../Button/Default/DefaultBtn"
 import Card from "../../UI/Card"
 import styles from "./SendLetter.module.css"
 
 const SendLetter = ({ onClose, card }) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
 
   const cancelClick = () => {
-    onClose();
+    onClose()
     console.log("취소로직")
   }
 
@@ -18,25 +18,35 @@ const SendLetter = ({ onClose, card }) => {
 
   const handleOutsideClick = (event) => {
     if (event.target === event.currentTarget) {
-      onClose();
+      onClose()
     }
   }
 
-  return (  
+  return (
     <div className={styles.overlay} onClick={handleOutsideClick}>
       <div className={styles.sendLetterContainer}>
         <Card className={styles.sendLetterBox}>
           <div className={styles.xmarkImg} onClick={cancelClick}>
             <img src="assets/icons/Pink_X-mark.png" alt="" />
           </div>
-          <img className={styles.topPostCardImg} src={`assets/images/post/${card.src}`}
+          <img
+            className={styles.topPostCardImg}
+            src={`assets/images/post/${card.src}`}
           />
           <div className={styles.titleInputContainer}>
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} maxLength={7} placeholder="제목 입력"
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              maxLength={7}
+              placeholder="제목 입력"
             />
           </div>
           <div className={styles.letterContent}>
-            <textarea value={content} onChange={(e) => setContent(e.target.value)} placeholder="편지 내용을 작성하세요."
+            <textarea
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              placeholder="편지 내용을 작성하세요."
             />
           </div>
         </Card>
