54d3507cd58a6fa6acf2f9a1d5a67ea87004ceab
김준형 <wnsgud6232@naver.com>
2023-10-27 23:06:30 +0900

    FEAT: 내 룸과 다른 사람 룸에 따라 CSS 다르게 적용

--- frontend/src/components/Modal/Post/PostDefaultModal.jsx
+++ frontend/src/components/Modal/Post/PostDefaultModal.jsx
@@ -5,14 +5,25 @@ const PostDefaultModal = (props) => {
   return (
     <>
       <div className={styles.overlay} onClick={props.cancelClick}></div>
-      <Card className={styles.container}>
-        <div className={styles.xmarkImg} onClick={props.cancelClick}>
-          <img src="assets/icons/Pink_X-mark.png" />
-        </div>
-        <div className={styles.containerTitle}>{props.PostDefaultTitle}</div>
-        <div className={styles.horizontalRule}></div>
-        <div className={props.className}>{props.children}</div>
-      </Card>
+      {props.postCheck ? (
+        <Card className={styles.containerOther}>
+          <div className={styles.xmarkImg} onClick={props.cancelClick}>
+            <img src="assets/icons/Pink_X-mark.png" />
+          </div>
+          <div className={styles.containerTitle}>{props.PostDefaultTitle}</div>
+          <div className={styles.horizontalRule}></div>
+          <div className={props.className}>{props.children}</div>
+        </Card>
+      ) : (
+        <Card className={styles.containerMy}>
+          <div className={styles.xmarkImg} onClick={props.cancelClick}>
+            <img src="assets/icons/Pink_X-mark.png" />
+          </div>
+          <div className={styles.containerTitle}>{props.PostDefaultTitle}</div>
+          <div className={styles.horizontalRule}></div>
+          <div className={props.className}>{props.children}</div>
+        </Card>
+      )}
     </>
   )
 }
