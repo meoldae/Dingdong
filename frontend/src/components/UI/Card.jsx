03465e12b918fa22e22aed109f0eb449e6f4646c
김준형 <wnsgud6232@naver.com>
2023-10-28 17:35:40 +0900

    CHORE: overlay로직 이동으로 인한 코드 제거

--- frontend/src/components/UI/Card.jsx
+++ frontend/src/components/UI/Card.jsx
@@ -2,12 +2,9 @@ import styles from "./Card.module.css"
 
 const Card = (props) => {
   return (
-    <>
-      <div className={styles.overlay} onClick={props.cancelClick}></div>
-      <div className={`${styles.CardContainer} ${props.className}`}>
-        {props.children}
-      </div>
-    </>
+    <div className={`${styles.CardContainer} ${props.className}`}>
+      {props.children}
+    </div>
   )
 }
 
