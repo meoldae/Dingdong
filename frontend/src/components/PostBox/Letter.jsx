c4f23858558b2c9cdb8a2ae726381570d26db4db
김준형 <wnsgud6232@naver.com>
2023-10-28 15:56:01 +0900

    CHORE: 편지지 아이콘 png -> svg로 변경

--- frontend/src/components/PostBox/Letter.jsx
+++ frontend/src/components/PostBox/Letter.jsx
@@ -2,7 +2,7 @@ import styles from "./Letter.module.css"
 const Letter = ({ letterTitle }) => {
   return (
     <div className={styles.letterContainer}>
-      <img src="assets/images/post/letter.png" alt="" />
+      <img src="assets/icons/writingPad.svg" alt="" />
       <div className={styles.letterTitle}>{letterTitle}</div>
     </div>
   )
