2911456fc9472b4ef9246230ad89df99731bc73e
김준형 <wnsgud6232@naver.com>
2023-10-28 22:01:09 +0900

    STYLE: 이미지 크기 고정

--- frontend/src/components/StampTest/StampTestStart.jsx
+++ frontend/src/components/StampTest/StampTestStart.jsx
@@ -5,8 +5,11 @@ import styles from "./StampTestStart.module.css"
 function StampTestStart(props) {
   return (
     <div className={styles.Container}>
-      <img src={"assets/images/stamp_start.svg"} />
-      <p>{`당신에게 어울리는 우표를 찾아보세요.`}</p>
+      <img
+        src={"assets/images/stamp_start.svg"}
+        style={{ width: "250px", height: "300px" }}
+      />
+      <p className={styles.dumy}>당신에게 어울리는 우표를 찾아보세요.</p>
       <div onClick={props.onTestModeHanlder}>
         <p>시작</p>
       </div>
