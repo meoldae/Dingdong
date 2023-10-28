cbfce6b5a0801c270989d96811d7e3ad86f946c8
김준형 <wnsgud6232@naver.com>
2023-10-28 16:42:55 +0900

    FEAT: PostDefaultModal Recoil 적용

--- frontend/src/components/Modal/Post/PostBox.jsx
+++ frontend/src/components/Modal/Post/PostBox.jsx
@@ -2,36 +2,42 @@ import { useEffect } from "react"
 import Letter from "../../PostBox/Letter"
 import styles from "./PostBox.module.css"
 import PostDefaultModal from "./PostDefaultModal"
+import { useRecoilValue } from "recoil"
+import { isPostBoxVisibleAtom } from "../../../atom/PostAtom"
 
 const PostBox = () => {
+  const isPostBoxVisible = useRecoilValue(isPostBoxVisibleAtom)
+
   return (
-    <PostDefaultModal
-      className={styles.postBoxContainer}
-      PostDefaultTitle={"편지함"}
-      postCheck={false}
-    >
-      <div className={styles.letterBox}>
-        <Letter letterTitle={"유저"} checkRead={true} />
-        <Letter letterTitle={"유저"} checkRead={false} />
-        <Letter letterTitle={"유저"} checkRead={true} />
-        <Letter letterTitle={"유저"} checkRead={true} />
-        <Letter letterTitle={"유저"} checkRead={false} />
-        <Letter letterTitle={"유저"} checkRead={false} />
-        <Letter letterTitle={"유저"} checkRead={true} />
-        <Letter letterTitle={"유저"} checkRead={true} />
-        <Letter letterTitle={"유저"} checkRead={false} />
-        <Letter letterTitle={"유저"} checkRead={true} />
-        <Letter letterTitle={"유저"} checkRead={false} />
-        <Letter letterTitle={"유저"} checkRead={true} />
-        <Letter letterTitle={"유저"} checkRead={false} />
-        <Letter letterTitle={"유저"} checkRead={true} />
-        <Letter letterTitle={"유저"} checkRead={false} />
-        <Letter letterTitle={"유저"} checkRead={true} />
-        <Letter letterTitle={"유저"} checkRead={true} />
-        <Letter letterTitle={"유저"} checkRead={true} />
-        <Letter letterTitle={"유저"} checkRead={false} />
-      </div>
-    </PostDefaultModal>
+    isPostBoxVisible && (
+      <PostDefaultModal
+        className={styles.postBoxContainer}
+        PostDefaultTitle={"편지함"}
+        postCheck={false}
+      >
+        <div className={styles.letterBox}>
+          <Letter letterTitle={"유저"} checkRead={true} />
+          <Letter letterTitle={"유저"} checkRead={false} />
+          <Letter letterTitle={"유저"} checkRead={true} />
+          <Letter letterTitle={"유저"} checkRead={true} />
+          <Letter letterTitle={"유저"} checkRead={false} />
+          <Letter letterTitle={"유저"} checkRead={false} />
+          <Letter letterTitle={"유저"} checkRead={true} />
+          <Letter letterTitle={"유저"} checkRead={true} />
+          <Letter letterTitle={"유저"} checkRead={false} />
+          <Letter letterTitle={"유저"} checkRead={true} />
+          <Letter letterTitle={"유저"} checkRead={false} />
+          <Letter letterTitle={"유저"} checkRead={true} />
+          <Letter letterTitle={"유저"} checkRead={false} />
+          <Letter letterTitle={"유저"} checkRead={true} />
+          <Letter letterTitle={"유저"} checkRead={false} />
+          <Letter letterTitle={"유저"} checkRead={true} />
+          <Letter letterTitle={"유저"} checkRead={true} />
+          <Letter letterTitle={"유저"} checkRead={true} />
+          <Letter letterTitle={"유저"} checkRead={false} />
+        </div>
+      </PostDefaultModal>
+    )
   )
 }
 
