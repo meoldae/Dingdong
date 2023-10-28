a0d5b7b3c5d5397dfd04a6f413a498661ad64aec
김준형 <wnsgud6232@naver.com>
2023-10-28 16:05:36 +0900

    STYLE: 안읽음표시 아이콘 추가

--- frontend/src/components/PostBox/Letter.jsx
+++ frontend/src/components/PostBox/Letter.jsx
@@ -2,6 +2,11 @@ import styles from "./Letter.module.css"
 const Letter = ({ letterTitle, checkRead }) => {
   return (
     <div className={styles.letterContainer}>
+      {checkRead ? (
+        <></>
+      ) : (
+        <img src="assets/icons/redDot.svg" className={styles.noRead} />
+      )}
       <img src="assets/icons/writingPad.svg" alt="" />
       <div className={styles.letterTitle}>{letterTitle}</div>
     </div>
