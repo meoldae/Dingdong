b449f845c2539ddb38d98bd3dade9643188e059c
김준형 <wnsgud6232@naver.com>
2023-10-28 23:30:18 +0900

    FEAT: 라우터 버그 문제 해결

--- frontend/src/components/StampTest/StampTestLoading.jsx
+++ frontend/src/components/StampTest/StampTestLoading.jsx
@@ -6,23 +6,23 @@ const StampTestLoading = (props) => {
 
   useEffect(() => {
     if (props.result === "IST") {
-      navigate("/yourstamp?result=0")
+      navigate("/yourstamp/result?result=0")
     } else if (props.result === "ISF") {
-      navigate("/yourstamp?result=1")
+      navigate("/yourstamp/result?result=1")
     } else if (props.result === "INF") {
-      navigate("/yourstamp?result=2")
+      navigate("/yourstamp/result?result=2")
     } else if (props.result === "INT") {
-      navigate("/yourstamp?result=3")
+      navigate("/yourstamp/result?result=3")
     } else if (props.result === "EST") {
-      navigate("/yourstamp?result=4")
+      navigate("/yourstamp/result?result=4")
     } else if (props.result === "ESF") {
-      navigate("/yourstamp?result=5")
+      navigate("/yourstamp/result?result=5")
     } else if (props.result === "ENF") {
-      navigate("/yourstamp?result=6")
+      navigate("/yourstamp/result?result=6")
     } else if (props.result === "ENT") {
-      navigate("/yourstamp?result=7")
+      navigate("/yourstamp/result?result=7")
     } else {
-      navigate("/yourstamp?result=0")
+      navigate("/yourstamp/result?result=0")
     }
   })
 
