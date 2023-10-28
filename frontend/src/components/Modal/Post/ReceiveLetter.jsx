c6dd3d385ae78b8decc7353821851c44665f4415
김준형 <wnsgud6232@naver.com>
2023-10-28 17:28:55 +0900

    FEAT: 편지 내용 추가

--- frontend/src/components/Modal/Post/ReceiveLetter.jsx
+++ frontend/src/components/Modal/Post/ReceiveLetter.jsx
@@ -10,13 +10,15 @@ const RecevieLetter = (props) => {
       <div className={styles.xmarkImg} onClick={props.cancelClick}>
         <img src="assets/icons/Pink_X-mark.png" alt="" />
       </div>
-
       <img
         className={styles.topPostCardImg}
         src="assets/images/post/postCardDefault.png"
       />
       <div className={styles.letterContent}>
         <h1>안녕 난 장호야</h1>
+      </div>
+      <div>
+        <span>편지 내용입니다.</span>
       </div>
       <div className={styles.sendUserName}>
         <h3>From.호~~~~!</h3>
