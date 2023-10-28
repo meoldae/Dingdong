416be532d3d8de9de76956cc1b2adac19ec29883
김준형 <wnsgud6232@naver.com>
2023-10-28 17:36:14 +0900

    FEAT: overlay 로직 이동

--- frontend/src/components/Modal/Post/ReceiveLetter.jsx
+++ frontend/src/components/Modal/Post/ReceiveLetter.jsx
@@ -3,27 +3,29 @@ import styles from "./ReceiveLetter.module.css"
 
 const RecevieLetter = (props) => {
   return (
-    <Card
-      className={styles.recevieLetterContainer}
-      cancelClick={props.cancelClick}
-    >
-      <div className={styles.xmarkImg} onClick={props.cancelClick}>
-        <img src="assets/icons/Pink_X-mark.png" alt="" />
-      </div>
-      <img
-        className={styles.topPostCardImg}
-        src="assets/images/post/postCardDefault.png"
-      />
-      <div className={styles.letterContent}>
-        <h1>안녕 난 장호야</h1>
-      </div>
-      <div>
-        <span>편지 내용입니다.</span>
-      </div>
-      <div className={styles.sendUserName}>
-        <h3>From.호~~~~!</h3>
-      </div>
-    </Card>
+    <div className={styles.overlay} onClick={props.cancelClick}>
+      <Card
+        className={styles.recevieLetterContainer}
+        cancelClick={props.cancelClick}
+      >
+        <div className={styles.xmarkImg} onClick={props.cancelClick}>
+          <img src="assets/icons/Pink_X-mark.png" alt="" />
+        </div>
+        <img
+          className={styles.topPostCardImg}
+          src="assets/images/post/postCardDefault.png"
+        />
+        <div className={styles.letterContent}>
+          <h1>안녕 난 장호야</h1>
+        </div>
+        <div>
+          <span>편지 내용입니다.</span>
+        </div>
+        <div className={styles.sendUserName}>
+          <h3>From.호~~~~!</h3>
+        </div>
+      </Card>
+    </div>
   )
 }
 
