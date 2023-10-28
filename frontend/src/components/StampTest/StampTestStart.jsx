f6bb75a66cae4e45757e7da3d6045872db188381
김준형 <wnsgud6232@naver.com>
2023-10-28 21:34:21 +0900

    FEAT: CSS적용을 위한 module css 파일 생성 및 적용

--- frontend/src/components/StampTest/StampTestStart.jsx
+++ frontend/src/components/StampTest/StampTestStart.jsx
@@ -1,23 +1,20 @@
-import React from 'react';  
-import SharingModalList from '../Modal/Sharing/SharingModalList';
+import React from "react"
+import SharingModalList from "../Modal/Sharing/SharingModalList"
+import styles from "./StampTestStart.module.css"
 
 function StampTestStart(props) {
   return (
-    <div> 
-      <div>
-        <p>우표 테스트</p>
-      </div>
+    <div className={styles.Container}>
       <p>{`당신에게 어울리는 우표를 찾아보세요.`}</p>
       <div onClick={props.onTestModeHanlder}>
-          <p>시작</p>        
+        <p>시작</p>
       </div>
       <div>
-            <hr/>
-            <p>테스트 공유하기</p>
-            <SharingModalList shareMode={'start'}/>
+        <p>테스트 공유하기</p>
+        <SharingModalList shareMode={"start"} />
       </div>
     </div>
-  );
+  )
 }
 
-export default StampTestStart;
+export default StampTestStart
