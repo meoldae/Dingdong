ac12c07ef641f23bbe0c657709a481c55914eac8
김준형 <wnsgud6232@naver.com>
2023-10-30 00:38:10 +0900

    FEAT: checkMyRoom의 값에 따라서 닉네임 다르게 표시

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import style from "./Header.module.css"
 import hamburger from "../../../public/assets/icons/hamburgerbar.svg"
 import bell from "../../../public/assets/icons/bell.svg"
-const Header = () => {
+
+const Header = ({ checkMyRoom }) => {
   const icon = "../../../../public/assets/icons/"
+
   return (
     <div className={style.wrap}>
       <div className={style.header}>
         <img src={hamburger} alt="" />
-        <div className={style.userName}>userName</div>
+        {checkMyRoom === "my" ? (
+          <div className={style.userName}>myRoom</div>
+        ) : (
+          <div className={style.userName}>userName</div>
+        )}
         <img src={bell} alt="" />
       </div>
     </div>
