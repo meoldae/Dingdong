2d2aa54cba84b9914c9058b9a2c05afda21fdacc
김준형 <wnsgud6232@naver.com>
2023-10-29 22:18:47 +0900

    FEAT: 상위 <> 제거 및 컴포넌트 추가 & 상태관리 추가 & 상태관리 함수 추가

--- frontend/src/pages/Post/PostPage.jsx
+++ frontend/src/pages/Post/PostPage.jsx
@@ -4,49 +4,50 @@ import { useState } from "react"
 // 스타일
 import styles from "./PostPage.module.css"
 
+// 컴포넌트
+import PostCardBox from "../../components/Modal/Post/PostCardBox"
+
 const PostPage = () => {
   const [searchText, setSearchText] = useState("")
+  const [isPostCardBox, setIsPostCardBox] = useState(false)
 
   const selectUserHandler = () => {
-    console.log("우표모달 띄우기")
+    setIsPostCardBox(true)
   }
 
   return (
-    <>
-      <div className={styles.Container}>
-        <div className={styles.SemiContainer}>
-          <div className={styles.TitleContainer}>
-            <div className={styles.Title}>우체국</div>
-            <div className={styles.SemiTitle}>
-              편지를 보낼 사람을 찾아보세요.
-            </div>
+    <div className={styles.Container}>
+      <div className={styles.SemiContainer}>
+        <div className={styles.TitleContainer}>
+          <div className={styles.Title}>우체국</div>
+          <div className={styles.SemiTitle}>편지를 보낼 사람을 찾아보세요.</div>
+        </div>
+        <div className={styles.TitleLine}></div>
+        <div className={styles.ContentContainer}>
+          <div className={styles.InputContainer}>
+            <input
+              type="text"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+              placeholder="검색"
+              maxLength={7}
+              className={styles.Input}
+            />
+            <img
+              src={"assets/icons/search.svg"}
+              className={styles.SearchIcon}
+            />
           </div>
-          <div className={styles.TitleLine}></div>
-          <div className={styles.ContentContainer}>
-            <div className={styles.InputContainer}>
-              <input
-                type="text"
-                value={searchText}
-                onChange={(e) => setSearchText(e.target.value)}
-                placeholder="검색"
-                maxLength={7}
-                className={styles.Input}
-              />
-              <img
-                src={"assets/icons/search.svg"}
-                className={styles.SearchIcon}
-              />
-            </div>
-            <div className={styles.ResultContainer}>
-              <div>유저 검색 결과</div>
-            </div>
-          </div>
-          <div className={styles.SelectButton} onClick={selectUserHandler}>
-            선택하기
+          <div className={styles.ResultContainer}>
+            <div>유저 검색 결과</div>
           </div>
         </div>
+        <div className={styles.SelectButton} onClick={selectUserHandler}>
+          선택하기
+        </div>
       </div>
-    </>
+      {isPostCardBox && <PostCardBox />}
+    </div>
   )
 }
 
