472ba6016bacde5b0672b46d0424ddb7393aebce
김준형 <wnsgud6232@naver.com>
2023-10-29 20:51:23 +0900

    STYLE: semi컨테이너를 이용해 내용 중간 정렬

--- frontend/src/pages/Post/PostPage.jsx
+++ frontend/src/pages/Post/PostPage.jsx
@@ -3,11 +3,14 @@ import styles from "./PostPage.module.css"
 const PostPage = () => {
   return (
     <div className={styles.Container}>
-      <div className={styles.TitleContainer}>
-        <div>주민 찾기</div>
-      </div>
-      <div className={styles.ContentContainer}>
-        <div>내용</div>
+      <div className={styles.SemiContainer}>
+        <div className={styles.TitleContainer}>
+          <div className={styles.Title}>우체국</div>
+          <div>편지를 보낼 사람을 찾아보세요.</div>
+        </div>
+        <div className={styles.ContentContainer}>
+          <div>내용</div>
+        </div>
       </div>
     </div>
   )
