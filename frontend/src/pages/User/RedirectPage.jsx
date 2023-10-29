d4f165075611c860c8f71a0eb590e6383f4139e9
Kang Dong Pyo <76652908+97Kzone@users.noreply.github.com>
2023-10-29 19:57:58 +0900

    FEAT: 로그인 처리 수정 - userAtom avatarId, nickname, roomId 추가

--- frontend/src/pages/User/RedirectPage.jsx
+++ frontend/src/pages/User/RedirectPage.jsx
@@ -1,18 +1,39 @@
 import { useRecoilState } from 'recoil';
 import { userAtom } from '@/atom/UserAtom';
 import { useNavigate } from 'react-router-dom';
+import { fetchUserInfo } from '../../api/User';
 
 const RedirectPage = () => {
     const navigate = useNavigate();
-    const [, setLoginInfo] = useRecoilState(userAtom);
+    const [loginInfo, setLoginInfo] = useRecoilState(userAtom);
 
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("token");
+    
+    useEffect(() => {
+        if (loginInfo.accessToken) {
+            fetchUserInfo(loginInfo.accessToken)
+                .then(response => {
+                    const avatarId = response.data.avatarId;
+                    const nickname = response.data.nickname;
+                    const roomId = response.data.roomId;
 
-    console.log(token);
+                    setLoginInfo(prevState => ({
+                        ...prevState,
+                        avatarId: avatarId,
+                        nickname: nickname,
+                        roomId: roomId
+                    }));
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            navigate("/");
+        }
+    }, [loginInfo.accessToken]); // accessToken이 변경될 때만 useEffect 내부가 실행됩니다.
+    
     if (token !== null) {
         setLoginInfo(prevState => ({ ...prevState, accessToken: token }));
-        navigate('/');
     }
 
     return null;
