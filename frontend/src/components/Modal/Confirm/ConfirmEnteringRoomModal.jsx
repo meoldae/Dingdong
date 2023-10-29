7e20b40248d24b0a2f2c7ccdce87bd9047857ad9
김준형 <wnsgud6232@naver.com>
2023-10-29 16:41:43 +0900

    FEAT: 모달 오픈 시, 글자가 순차적으로 나오는 로직 작성

--- frontend/src/components/Modal/Confirm/ConfirmEnteringRoomModal.jsx
+++ frontend/src/components/Modal/Confirm/ConfirmEnteringRoomModal.jsx
@@ -45,18 +45,60 @@ const ConfirmEnteringRoomModal = () => {
     setIsInitialRender(true)
   }
 
+  // 글자가 작성되는 JS
+  const [content, setContent] = useState("")
+  const [yes, setYes] = useState("")
+  const [no, setNo] = useState("")
+
+  const letters = "마이룸에 입장하시겠습니까?"
+  const yesText = ["▶", " ", "예"]
+  const noText = ["▶", " ", "아니오"]
+
+  const speed = 50
+  const delay = 200
+
+  const wait = (ms) => new Promise((res) => setTimeout(res, ms))
+
+  useEffect(() => {
+    const typeYes = async () => {
+      for (let char of yesText) {
+        setYes((prevText) => prevText + char)
+        await wait(speed)
+      }
+      await wait(delay)
+    }
+
+    const typeNo = async () => {
+      for (let char of noText) {
+        setNo((prevText) => prevText + char)
+        await wait(speed)
+      }
+    }
+
+    const typeWords = async () => {
+      for (let char of letters) {
+        setContent((prevText) => prevText + char)
+        await wait(speed)
+      }
+      await wait(delay)
+      await typeYes()
+      await typeNo()
+    }
+    typeWords()
+  }, [])
+
   return (
     <div className={styles.MainModal}>
       <div className={styles.ContentBox}>
         <div className={styles.ContentContainer}>
           <div className={styles.Title}>딩동!</div>
-          <div className={styles.Content}>마이룸에 입장하시겠습니까?</div>
+          <div className={styles.Content}>{content}</div>
           <div className={styles.ConfirmContainer}>
             <div className={styles.Confirm} onClick={onConfirm}>
-              ▶&nbsp;&nbsp;&nbsp;예
+              {yes}
             </div>
             <div className={styles.Confirm} onClick={onCancle}>
-              ▶&nbsp;&nbsp;&nbsp;아니오
+              {no}
             </div>
             {/* &nbsp; = 띄어쓰기 */}
           </div>
