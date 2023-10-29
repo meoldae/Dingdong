f6b11c325b0d53c33b9c0a21d473fcf8a8aa684a
김준형 <wnsgud6232@naver.com>
2023-10-29 20:40:00 +0900

    FEAT: 페이지 틀 구성

--- frontend/src/pages/Post/PostPage.jsx
+++ frontend/src/pages/Post/PostPage.jsx
@@ -3,7 +3,12 @@ import styles from "./PostPage.module.css"
 const PostPage = () => {
   return (
     <div className={styles.Container}>
-      <div>주민 찾기</div>
+      <div className={styles.TitleContainer}>
+        <div>주민 찾기</div>
+      </div>
+      <div className={styles.ContentContainer}>
+        <div>내용</div>
+      </div>
     </div>
   )
 }
