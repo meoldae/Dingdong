264e62d7ca66b1f1976d17c0d6856bbad0097092
김준형 <wnsgud6232@naver.com>
2023-10-29 22:15:59 +0900

    CHORE: 컴포넌트 사용을 위해 상위 <> 생성

--- frontend/src/pages/Post/PostPage.jsx
+++ frontend/src/pages/Post/PostPage.jsx
@@ -12,37 +12,41 @@ const PostPage = () => {
   }
 
   return (
-    <div className={styles.Container}>
-      <div className={styles.SemiContainer}>
-        <div className={styles.TitleContainer}>
-          <div className={styles.Title}>우체국</div>
-          <div className={styles.SemiTitle}>편지를 보낼 사람을 찾아보세요.</div>
-        </div>
-        <div className={styles.TitleLine}></div>
-        <div className={styles.ContentContainer}>
-          <div className={styles.InputContainer}>
-            <input
-              type="text"
-              value={searchText}
-              onChange={(e) => setSearchText(e.target.value)}
-              placeholder="검색"
-              maxLength={7}
-              className={styles.Input}
-            />
-            <img
-              src={"assets/icons/search.svg"}
-              className={styles.SearchIcon}
-            />
+    <>
+      <div className={styles.Container}>
+        <div className={styles.SemiContainer}>
+          <div className={styles.TitleContainer}>
+            <div className={styles.Title}>우체국</div>
+            <div className={styles.SemiTitle}>
+              편지를 보낼 사람을 찾아보세요.
+            </div>
           </div>
-          <div className={styles.ResultContainer}>
-            <div>유저 검색 결과</div>
+          <div className={styles.TitleLine}></div>
+          <div className={styles.ContentContainer}>
+            <div className={styles.InputContainer}>
+              <input
+                type="text"
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                placeholder="검색"
+                maxLength={7}
+                className={styles.Input}
+              />
+              <img
+                src={"assets/icons/search.svg"}
+                className={styles.SearchIcon}
+              />
+            </div>
+            <div className={styles.ResultContainer}>
+              <div>유저 검색 결과</div>
+            </div>
           </div>
-        </div>
-        <div className={styles.SelectButton} onClick={selectUserHandler}>
-          선택하기
+          <div className={styles.SelectButton} onClick={selectUserHandler}>
+            선택하기
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
