81533b0eb4c0ff3f3310d9a895fca45a07bce96c
Kang Dong Pyo <76652908+97Kzone@users.noreply.github.com>
2023-10-29 20:16:00 +0900

    FIX: 로그인 로직 수정

--- frontend/src/pages/User/RedirectPage.jsx
+++ frontend/src/pages/User/RedirectPage.jsx
@@ -31,7 +31,7 @@ const RedirectPage = () => {
                     console.log(error);
                 });
             
-            navigate("/");
+            // navigate("/");
         }
     }, [loginInfo.accessToken]); 
     
