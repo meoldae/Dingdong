a3d5422df047e315b48f14334456721559e23693
Junyoung <wnsdud12365@naver.com>
2023-10-30 21:15:18 +0900

    FIX: 액세스토큰 30분으로 복구

--- backend/src/main/java/com/ssafy/dingdong/global/util/JwtProvider.java
+++ backend/src/main/java/com/ssafy/dingdong/global/util/JwtProvider.java
@@ -28,9 +28,7 @@ public class JwtProvider {
 	@Value("${auth.secretKey}")
 	private String SECRET_KEY;
 
-	//    private static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 60 * 30; // 30분
-	// private static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 60 * 60 * 24; // 테스트용 24시간
-	private static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 30; // 리프레시 테스트용 30초
+	private static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 60 * 30; // 30분
 	private static final Long REFRESH_TOKEN_VALIDATE_TIME = 1000L * 60 * 60 * 24 * 365; // 1년
 
 	public String createAccessToken(Member member) {
