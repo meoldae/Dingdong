b242becc093d6717a3434414a043d0440ebc5089
Junyoung <wnsdud12365@naver.com>
2023-10-30 20:34:46 +0900

    REFACTOR: 리프레시 테스트용 액세스 토큰 1분 처리

--- backend/src/main/java/com/ssafy/dingdong/global/util/JwtProvider.java
+++ backend/src/main/java/com/ssafy/dingdong/global/util/JwtProvider.java
@@ -30,7 +30,7 @@ public class JwtProvider {
 
 	//    private static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 60 * 30; // 30분
 	// private static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 60 * 60 * 24; // 테스트용 24시간
-	private static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 60 * 3; // 리프레시 테스트용 3분
+	private static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 60 * 1; // 리프레시 테스트용 1분
 	private static final Long REFRESH_TOKEN_VALIDATE_TIME = 1000L * 60 * 60 * 24 * 365; // 1년
 
 	public String createAccessToken(Member member) {
