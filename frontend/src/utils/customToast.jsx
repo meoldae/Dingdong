f94dd8ba50c35b095621a2572f9f2245fb23c916
김준형 <wnsgud6232@naver.com>
2023-10-30 17:00:21 +0900

    FEAT: 이웃 요청 커스텀 토스트 생성

--- frontend/src/utils/customToast.jsx
+++ frontend/src/utils/customToast.jsx
@@ -1,10 +1,17 @@
-import toast, { Toaster } from 'react-hot-toast';
-import './customToast.module.css'
+import toast, { Toaster } from "react-hot-toast"
+import "./customToast.module.css"
 
-export const successMsg = (msg) => toast(msg);
+export const successMsg = (msg) => toast(msg)
 
 export const CustomToast = () => {
-    return (
-        <Toaster toastOptions={{className : "custom-toast", duration: 3000}}/>
-    )
+  return (
+    <Toaster toastOptions={{ className: "custom-toast", duration: 3000 }} />
+  )
+}
+
+export const neighborRequestMsg = (msg) => toast(msg)
+export const NeighborRequestToast = () => {
+  return (
+    <Toaster toastOptions={{ className: "neighbor-toast", duration: 3000 }} />
+  )
 }
