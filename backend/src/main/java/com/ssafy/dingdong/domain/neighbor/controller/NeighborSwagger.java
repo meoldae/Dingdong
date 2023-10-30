428688dbbef6974daed96cd5aad897651c2b6130
Junyoung <wnsdud12365@naver.com>
2023-10-30 19:32:19 +0900

    FEAT: 이웃 끊기 메서드 구현 완료

--- backend/src/main/java/com/ssafy/dingdong/domain/neighbor/controller/NeighborSwagger.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/neighbor/controller/NeighborSwagger.java
@@ -1,12 +1,15 @@
 package com.ssafy.dingdong.domain.neighbor.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.ssafy.dingdong.domain.neighbor.dto.request.DeleteNeighborRequestDto;
 import com.ssafy.dingdong.domain.neighbor.dto.request.NeighborRequest;
 import com.ssafy.dingdong.domain.neighbor.dto.response.NeighborResponse;
 import com.ssafy.dingdong.domain.neighbor.entity.Neighbor;
@@ -24,4 +27,6 @@ public interface NeighborSwagger {
 	DataResponse<List<NeighborResponse>> getNeighborList(Authentication authentication);
 
 	CommonResponse setNeighborStatus(@Validated @RequestBody NeighborRequest neighborRequest);
+
+	CommonResponse deleteNeighbor(@RequestBody Map<String, Object> paramMap, Authentication authentication);
 }
