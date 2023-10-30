75091beeb737338abbb50d8405c55d69fec4d6e2
김준형 <wnsgud6232@naver.com>
2023-10-30 12:53:00 +0900

    FEAT: 이웃 추가 API 코드 작성

--- frontend/src/components/Header/NeighborRequest.jsx
+++ frontend/src/components/Header/NeighborRequest.jsx
@@ -1,9 +1,25 @@
+// 스타일
 import style from "./Header.module.css"
+
+// 컴포넌트
 import RoomBtn from "../Button/Room/RoomBtn"
 
+// API
+import { fetchNeighrborAdd } from "@/api/Neighbor"
+
 const NeighborRequest = () => {
+  // 이웃 추가하는 함수
+  // targetId = roomId로 변경될 예정..! 수정되면 해당 주석 지워주세요.
   const isNeighbor = () => {
-    console.log("이웃추가?")
+    fetchNeighrborAdd(
+      targetId,
+      (response) => {
+        console.log(response.data.data)
+      },
+      (error) => {
+        console.log("Error at fetching Neighbor Add...", error)
+      }
+    )
   }
 
   return (
