c2a89af80e06a3e0326ff8447f2d053be6c8fedb
Dayoung1014 <58163364+Dayoung1014@users.noreply.github.com>
2023-10-31 21:03:05 +0900

    FIX: 게스트 footer

--- frontend/src/components/Footer/InviteFooter.jsx
+++ frontend/src/components/Footer/InviteFooter.jsx
@@ -3,13 +3,19 @@ import PostCardBox from "../Modal/Post/PostCardBox"
 import RoomBtn from "../Button/Room/RoomBtn"
 import SendLetter from "../Modal/Post/SendLetter"
 import style from "./Footer.module.css" 
+import { useNavigate } from "react-router-dom" 
 
 const OtherFooter = (props) => {
+  const navigate = useNavigate()
   const [isModalVisible, setIsModalVisible] = useState(false)
   const [isSendLetterModalVisible, setIsSendLetterModalVisible] =
     useState(false)
   const [selectedPostCard, setSelectedPostCard] = useState(null)
   const [isHeart, setIsHeart] = useState(false);
+
+  const onHomeHandler = (e) => {
+    navigate("/login")
+  }
 
   const openModal = () => {
     setIsModalVisible(true)
@@ -38,6 +44,10 @@ const OtherFooter = (props) => {
       <div className={style.inviteFooter}> 
         <div className={style.background}>
           <div>
+            <div className={style.circle} onClick={onHomeHandler}>
+              <img src={"/assets/icons/post.png"} className={style.iconImage}/>
+              <p>딩동 즐기러 가기</p>
+            </div>
             <div className={style.circle} onClick={openModal}>
               <img src={"/assets/icons/post.png"} className={style.iconImage}/>
               <p>편지 남기기</p>
