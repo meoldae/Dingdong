301c9fa4b60a962474f072836b353dcabb3aea84
김준형 <wnsgud6232@naver.com>
2023-10-31 10:48:33 +0900

    FEAT: 로딩페이지 구성 완료 & react-spinners 설치

--- frontend/src/pages/Loading/LoadingPage.jsx
+++ frontend/src/pages/Loading/LoadingPage.jsx
@@ -1,10 +1,15 @@
+// 라이브러리
+import React from "react"
+import { SyncLoader } from "react-spinners"
+
 // 스타일
 import styles from "./LoadingPage.module.css"
 
 const LoadingPage = () => {
   return (
     <div className={styles.Container}>
-      <div className={styles.Content}>로딩중..!</div>
+      <div className={styles.Content}>잠시만 기다려주세요.</div>
+      <SyncLoader />
     </div>
   )
 }
