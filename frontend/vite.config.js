294e12974ef842b037b29547448edf7927ad6e32
Junyoung <wnsdud12365@naver.com>
2023-11-01 17:28:44 +0900

    BUILD: Vite BaseURL 설정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
+  base: 'https://ding-dong.kr/feDev/',
   plugins: [
     react(),
     VitePWA({
