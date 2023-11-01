cc501d88c750abf44ba20ad110f29b0ee881f028
Junyoung <wnsdud12365@naver.com>
2023-11-01 21:52:55 +0900

    FIX: VITE Config 파일 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
-  base: "/feDev",
+  base: "/feDev/",
   plugins: [
     react(),
     VitePWA({
