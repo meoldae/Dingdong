1df10d46e46e0a5e6ddd3c57cbdc6f9b91cc7062
김준형 <wnsgud6232@naver.com>
2023-11-02 04:22:09 +0900

    COMMENT: 주석 작성

--- frontend/src/components/Modal/Ranking/RankingModal.jsx
+++ frontend/src/components/Modal/Ranking/RankingModal.jsx
@@ -8,6 +8,7 @@ import { fetchScore } from "../../../api/Score"
 import styles from "./RankingModal.module.css"
 
 const RankingModal = () => {
+  // URL 이동경로
   const urlPath = import.meta.env.VITE_APP_ROUTER_URL
   // 기준시간 상태관리
   const [standardTime, setStandardTime] = useState("")
