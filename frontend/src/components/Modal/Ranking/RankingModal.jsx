a48fba5ae0c63809c0282f40385b692e0f9268ba
김준형 <wnsgud6232@naver.com>
2023-11-01 15:56:05 +0900

    FEAT: 랭킹모달을 위한 컴포넌트/CSS 파일 생성

---
+++ frontend/src/components/Modal/Ranking/RankingModal.jsx
@@ -0,0 +1,12 @@
+// 스타일
+import styles from "./RankingModal.module.css"
+
+const RankingModal = () => {
+  return (
+    <>
+      <div className={styles.Container}>랭킹모달</div>
+    </>
+  )
+}
+
+export default RankingModal
