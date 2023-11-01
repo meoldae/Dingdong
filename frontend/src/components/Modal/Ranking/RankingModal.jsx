431990a8c418514c535835ef78eeeb3948f823e9
김준형 <wnsgud6232@naver.com>
2023-11-01 16:27:00 +0900

    CHORE: 제목 스타일 컨테이너 생성

--- frontend/src/components/Modal/Ranking/RankingModal.jsx
+++ frontend/src/components/Modal/Ranking/RankingModal.jsx
@@ -4,7 +4,11 @@ import styles from "./RankingModal.module.css"
 const RankingModal = () => {
   return (
     <>
-      <div className={styles.Container}>랭킹모달</div>
+      <div className={styles.Container}>
+        <div className={styles.TitleContainer}>
+          랭킹모달
+        </div>
+      </div>
     </>
   )
 }
