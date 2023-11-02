4cb67ec67845d0f7072138678b9803396f5dec76
Junyoung <wnsdud12365@naver.com>
2023-11-02 17:29:28 +0900

    FIX: Validation 최소 닉네임 길이 1자로 변경

--- backend/src/main/java/com/ssafy/dingdong/domain/member/dto/request/MemberSignUpDto.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/member/dto/request/MemberSignUpDto.java
@@ -8,7 +8,7 @@ import org.hibernate.validator.constraints.Length;
 public record MemberSignUpDto(
 	String memberId,
 	@NotBlank(message = "닉네임은 필수 항목입니다.")
-	@Length(min = 2, max = 8, message = "닉네임은 2 ~ 8 글자 사이입니다.")
+	@Length(min = 1, max = 8, message = "닉네임은 1 ~ 8 글자 사이입니다.")
 	String nickname,
 	@NotNull(message = "캐릭터는 필수 선택항목입니다.")
 	Long avatarId
