74ed183a3c4bd01206aae197699e8f72b4b801c3
김준형 <wnsgud6232@naver.com>
2023-11-02 11:00:06 +0900

    CHORE: 사용안하는 라이브러리 제거

--- frontend/src/pages/SinglePlay/SingleMainPage.jsx
+++ frontend/src/pages/SinglePlay/SingleMainPage.jsx
@@ -1,7 +1,7 @@
 // React
 import React, { useEffect, useState } from "react"
 import styles from "./SingleMainPage.module.css"
-import { motion, AnimatePresence } from "framer-motion"
+import { motion } from "framer-motion"
 
 // Recoil
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil"
@@ -366,14 +366,12 @@ const SingleMainPage = () => {
 
       {/* 랭킹모달 */}
       {confirmEnteringRank && (
-        <AnimatePresence>
-          <motion.div initial={{opacity: 0}} animate={{opacity:1}} transition={{delay: 1, duration: 0.5}}>
-            <div className={styles.overlay} onClick={() => closeRanking()} />
-            <div className={styles.rankingModalContainer}>
-              <RankingModal />
-            </div>
-          </motion.div>
-        </AnimatePresence>
+        <motion.div initial={{opacity: 0}} animate={{opacity:1}} transition={{delay: 1, duration: 0.5}}>
+          <div className={styles.overlay} onClick={() => closeRanking()} />
+          <div className={styles.rankingModalContainer}>
+            <RankingModal />
+          </div>
+        </motion.div>
       )}
     </>
   )
