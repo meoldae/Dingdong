eb873802cee18d478759f2e817db79c91b8b9451
김준형 <wnsgud6232@naver.com>
2023-11-03 13:28:29 +0900

    CHORE: import문 주석 작성 및 정리

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import ReactDOM from "react-dom/client"
-import { RecoilRoot } from "recoil"
-import { CustomToast } from "./utils/customToast.jsx"
-import AppRouter from "./router/AppRouter.jsx"
+// 폰트
 import "./fonts/Fonts.css"
+
+// 스타일
 import "./App.css"
 
+// 라이브러리
+import { useEffect } from "react"
+import { RecoilRoot } from "recoil"
+import ReactDOM from "react-dom/client"
+
+// 컴포넌트
+import { CustomToast } from "./utils/customToast.jsx"
+import AppRouter from "./router/AppRouter.jsx"
+
 const AppWrapper = () => {
+  useEffect(() => {
+    
+  }, [])
+
   return (
     <div className="appWrapper">
       <AppRouter />
