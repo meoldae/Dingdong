14b02efd9fe8fd66aa1dff3b8f97b0545d772b3e
KyongBeom <dragontig98@naver.com>
2023-11-05 22:17:22 +0900

    FEAT: 편지함 그리드 변경

--- frontend/src/components/Modal/Post/ReceiveLetter.jsx
+++ frontend/src/components/Modal/Post/ReceiveLetter.jsx
@@ -1,93 +1,108 @@
-import Card from "../../UI/Card"
-import styles from "./ReceiveLetter.module.css"
-import { letterIdAtom } from "@/atom/LetterAtom"
-import { useRecoilValue } from "recoil"
-import { useState, useEffect } from "react"
-import { getLetterDetail } from "@/api/Letter"
-import { successMsg } from "../../../utils/customToast"
-import { reportLetter } from "../../../api/Letter"
-import DefaultModal from "../Default/DefaultModal"
+import Card from "../../UI/Card";
+import styles from "./ReceiveLetter.module.css";
+import { letterIdAtom } from "@/atom/LetterAtom";
+import { useRecoilValue } from "recoil";
+import { useState, useEffect } from "react";
+import { getLetterDetail } from "@/api/Letter";
+import { successMsg } from "../../../utils/customToast";
+import { reportLetter } from "../../../api/Letter";
+import DefaultModal from "../Default/DefaultModal";
 
 const RecevieLetter = (props) => {
-  const letterId = useRecoilValue(letterIdAtom)
+  const letterId = useRecoilValue(letterIdAtom);
 
-  const [letterDetail, setLetterDetail] = useState(null)
+  const [letterDetail, setLetterDetail] = useState(null);
   // 편지함 종료모달 상태관리
-  const [isFinishReceiveLetter, setIsFinishReceiveLetter] = useState(false)
+  const [isFinishReceiveLetter, setIsFinishReceiveLetter] = useState(false);
 
-  const urlPath = import.meta.env.VITE_APP_ROUTER_URL
+  const urlPath = import.meta.env.VITE_APP_ROUTER_URL;
 
   useEffect(() => {
     const fetchLetterDetail = async () => {
       try {
         getLetterDetail(letterId, (response) => {
           if (response.data.code === "SUCCESS") {
-            setLetterDetail(response.data.data)
+            setLetterDetail(response.data.data);
           }
-        })
+        });
       } catch (error) {
-        console.error("Error fetching letter details:", error)
+        console.error("Error fetching letter details:", error);
       }
-    }
+    };
 
-    fetchLetterDetail()
-  }, [])
+    fetchLetterDetail();
+  }, []);
 
   const reportHandler = () => {
     reportLetter(
       letterId,
       (success) => {
-        successMsg("🚫 신고하기 완료!")
+        successMsg("🚫 신고하기 완료!");
       },
       (error) => {
-        'Error at reportLetter...', error
+        "Error at reportLetter...", error;
       }
-    )
-  }
+    );
+  };
 
   return (
     <>
-      <div className={styles.overlay} onClick={() => setIsFinishReceiveLetter(true)}>
-        {letterDetail ? (
-          <div className={styles.receiveLetterContainer}>
-            <Card className={styles.receiveLetterBox}>
-              <div className={styles.xmarkImg} onClick={() => setIsFinishReceiveLetter(true)}>
-                <img src={`${urlPath}/assets/icons/Pink_X-mark.png`} alt="" />
-              </div>
-              <img
-                className={styles.topPostCardImg}
-                src={`${urlPath}/assets/images/post/${letterDetail?.stampImgUrl
-                  .split("/")
-                  .pop()}`}
+      <div
+        className={styles.overlay}
+        onClick={() => setIsFinishReceiveLetter(true)}
+      >
+        <div className={styles.receiveLetterContainer}>
+          <Card className={styles.receiveLetterBox}>
+            <div
+              className={styles.xmarkImg}
+              onClick={() => setIsFinishReceiveLetter(true)}
+            >
+              <img src={`${urlPath}/assets/icons/Pink_X-mark.png`} alt="" />
+            </div>
+            <img
+              className={styles.topPostCardImg}
+              src={`${urlPath}/assets/images/post/${letterDetail?.stampImgUrl
+                .split("/")
+                .pop()}`}
+            />
+            <div
+              className={styles.letterToUser}
+              style={{ fontFamily: "GangwonEduAll-Light" }}
+            >
+              To. {letterDetail?.letterTo}
+            </div>
+            <div
+              className={styles.letterContent}
+              style={{ fontFamily: "GangwonEduAll-Light" }}
+            >
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: letterDetail?.description.replaceAll("\n", "<br />"),
+                }}
               />
-              <div className={styles.letterToUser} style={{ fontFamily: "GangwonEduAll-Light" }}>
-                To. {letterDetail?.letterTo}
+            </div>
+            <div
+              className={styles.footerContainer}
+              style={{ fontFamily: "GangwonEduAll-Light" }}
+            >
+              <div className={styles.report} onClick={reportHandler}>
+                신고하기
               </div>
-              <div className={styles.letterContent} style={{ fontFamily: "GangwonEduAll-Light" }}>
-              <span dangerouslySetInnerHTML={{ __html: letterDetail?.description.replaceAll('\n', '<br />') }} />
+              <div className={styles.FromUser}>
+                From. {letterDetail?.letterFrom}
               </div>
-              <div className={styles.footerContainer} style={{ fontFamily: "GangwonEduAll-Light" }}>
-                <div className={styles.report} onClick={reportHandler}>
-                  신고하기
-                </div>
-                <div className={styles.FromUser}>
-                  From. {letterDetail?.letterFrom}
-                </div>
-              </div>
-            </Card>
-          </div>
-        ):(
-          <div>
-            편지가 없습니다.
-          </div>
-        )
-        }
+            </div>
+          </Card>
+        </div>
       </div>
 
       {/* 받은편지 종료모달 */}
       {isFinishReceiveLetter && (
         <>
-          <div className={styles.finishOverlay} onClick={() => setIsFinishReceiveLetter(false)}>
+          <div
+            className={styles.finishOverlay}
+            onClick={() => setIsFinishReceiveLetter(false)}
+          >
             <div className={styles.finishContainer}>
               <DefaultModal
                 content={"편지를 종료하시겠습니까?"}
@@ -101,7 +116,7 @@ const RecevieLetter = (props) => {
         </>
       )}
     </>
-  )
-}
+  );
+};
 
-export default RecevieLetter
+export default RecevieLetter;
