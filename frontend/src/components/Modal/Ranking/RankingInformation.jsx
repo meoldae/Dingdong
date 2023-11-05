8de27d2c054e36aaa92e769d2ce12f4c9e692b16
김준형 <wnsgud6232@naver.com>
2023-11-05 16:05:17 +0900

    FEAT: 제목 생성 및 세미컨테이너 생성

--- frontend/src/components/Modal/Ranking/RankingInformation.jsx
+++ frontend/src/components/Modal/Ranking/RankingInformation.jsx
@@ -3,7 +3,11 @@ import styles from "./RankingInformation.module.css"
 const RankingInformation = () => {
   return (
     <>
-      <div className={styles.Container}>랭킹모달</div>
+      <div className={styles.Container}>
+        <div className={styles.SemiContainer}>
+          <div>순위권에 들어가는 꿀팁!</div>
+        </div>
+      </div>
     </>
   )
 }
