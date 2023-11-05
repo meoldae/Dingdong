6974e3a5be6d35888ece70bd4c414819c5b681f8
김준형 <wnsgud6232@naver.com>
2023-11-05 15:34:32 +0900

    FEAT: 랭킹 정보 모달 기본 틀 작성

---
+++ frontend/src/components/Modal/Ranking/RankingInformation.jsx
@@ -0,0 +1,11 @@
+import styles from "./RankingInformation.module.css"
+
+const RankingInformation = () => {
+  return (
+    <>
+      <div className={styles.Container}>랭킹모달</div>
+    </>
+  )
+}
+
+export default RankingInformation
