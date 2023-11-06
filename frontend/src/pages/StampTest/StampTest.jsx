2efd4cf114ffcf171d210e880b62230461ab20fb
rlawkdgh0406@naver.com <rlawkdgh0406@naver.com>
2023-11-06 14:41:07 +0900

    FEAT: SNS 기능 추가

--- frontend/src/pages/StampTest/StampTest.jsx
+++ frontend/src/pages/StampTest/StampTest.jsx
@@ -1,9 +1,34 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import StampTestOptions from "@/components/StampTest/StampTestOptions"
 import StampTestStart from "@/components/StampTest/StampTestStart"
-import StampTestLoading from "@/components/StampTest/StampTestLoading" 
+import StampTestLoading from "@/components/StampTest/StampTestLoading"
+import { useNavigate } from "react-router-dom"
+import history from "../../components/UI/history"
 
 const StampTest = () => {
+  // 브라우저 뒤로가기 버튼 처리
+  const [locationKeys, setLocationKeys] = useState([])
+  const navigate = useNavigate()
+  const urlPath = import.meta.env.VITE_APP_ROUTER_URL
+
+  useEffect(() => {
+    return history.listen((location) => {
+      if (history.action === "PUSH") {
+        setLocationKeys([location.key])
+        window.location.replace(`${urlPath}/`)
+      }
+
+      if (history.action === "POP") {
+        if (locationKeys[1] === location.key) {
+          setLocationKeys(([_, ...keys]) => keys)
+          window.location.replace(`${urlPath}/`)
+        } else {
+          setLocationKeys((keys) => [location.key, ...keys])
+        }
+      }
+    })
+  }, [locationKeys, history])
+
   const [testMode, setTestMode] = useState("start")
   const [result, setResult] = useState("")
 
