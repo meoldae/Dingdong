77102d5ebe6a37c611f5a4188dc54d7b59f1a28a
김준형 <wnsgud6232@naver.com>
2023-11-06 10:19:43 +0900

    CHORE: 필요없는 코드 제거

--- frontend/src/pages/Postoffice/PostofficeDefaultModal.jsx
+++ frontend/src/pages/Postoffice/PostofficeDefaultModal.jsx
@@ -4,13 +4,12 @@ import styles from "./PostofficeDefaultModal.module.css"
 import { useRecoilState } from "recoil";
 const PostDefaultModal = (props) => {
   const [onPostofficeCard, setOnPostOfficeCard] =
-    useRecoilState(postofficeCardAtom); 
+    useRecoilState(postofficeCardAtom);
   const urlPath = import.meta.env.VITE_APP_ROUTER_URL
   
   return (
     <>
       <div className={styles.overlay} onClick={()=>{setOnPostOfficeCard(false)}}/>
-      {onPostofficeCard ? (
         <Card className={styles.containerOther}>
           <div className={styles.xmarkImg} onClick={()=>{setOnPostOfficeCard(false)}}>
             <img src={`${urlPath}/assets/icons/x.png`} />
@@ -19,16 +18,6 @@ const PostDefaultModal = (props) => {
           <div className={styles.horizontalRule}></div>
           <div className={props.className}>{props.children}</div>
         </Card>
-      ) : (
-        <Card className={styles.containerMy}>
-          <div className={styles.xmarkImg} onClick={()=>{setOnPostOfficeCard(false)}}>
-            <img src={`${urlPath}/assets/icons/x.png`} />
-          </div>
-          <div className={styles.containerTitle}>{props.PostDefaultTitle}</div>
-          <div className={styles.horizontalRule}></div>
-          <div className={props.className}>{props.children}</div>
-        </Card>
-      )}
     </>
   )
 }
