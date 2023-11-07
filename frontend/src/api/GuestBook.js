ba7adf80a342b2bbab7d2295b728638dd926a404
김준형 <wnsgud6232@naver.com>
2023-11-07 17:19:44 +0900

    FEAT: 방명록 리스트 가져오는 API함수 작성

--- frontend/src/api/GuestBook.js
+++ frontend/src/api/GuestBook.js
@@ -5,4 +5,9 @@ const fetchWriteGuestBook = async (params, success, fail) => {
     await HttpJson.post(`visitorbook`, params).then(success).catch(fail);
 };
 
-export { fetchWriteGuestBook };
+// 방명록 작성
+const fetchListGuestBook = async (roomId, success, fail) => {
+    await HttpJson.get(`visitorbook/list/${roomId}`).then(success).catch(fail);
+};
+
+export { fetchWriteGuestBook, fetchListGuestBook };
