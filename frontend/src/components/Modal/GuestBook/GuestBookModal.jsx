eef4d95f3b7ce6d727bbb3ccb7c264de4456902b
김준형 <wnsgud6232@naver.com>
2023-11-07 14:53:40 +0900

    FEAT: ContentItem 컴포넌트화 진행

--- frontend/src/components/Modal/GuestBook/GuestBookModal.jsx
+++ frontend/src/components/Modal/GuestBook/GuestBookModal.jsx
@@ -2,16 +2,32 @@
 import styles from "./GuestBookModal.module.css"
 
 const GuestBookModal = () => {
+  // url 경로
+  const urlPath = import.meta.env.VITE_APP_ROUTER_URL
+
+  // 콘텐츠 아이템
+  const ContentItem = ({ content }) => {
+    return (
+      <div className={styles.Item}>
+        <div className={styles.Content}>
+          <div className={styles.temp}>
+            {content}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
       <div className={styles.Container}>
         <div className={styles.Title}>To. 다영시치</div>
         <div className={styles.ContentContainer}>
-          콘텐츠
+          <ContentItem content={"이건 테스트를 위한 더미 내용입니다. 진짜 신기하죠? 우와오아와와와와!"} />
         </div>
         <div className={styles.FooterContainer}>
           <div className={styles.WriteButton}>
-            +
+            <img src={`${urlPath}/assets/icons/writeGuestBook.png`} style={{ width: "20px", height: "20px" }} />
           </div>
         </div>
       </div>
