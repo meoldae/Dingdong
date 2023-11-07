305ba301894edabe538d163de7777349eda208a3
김준형 <wnsgud6232@naver.com>
2023-11-07 15:11:27 +0900

    FEAT: 색상 스타일 리스트로 작성

--- frontend/src/components/Modal/GuestBook/GuestBookModal.jsx
+++ frontend/src/components/Modal/GuestBook/GuestBookModal.jsx
@@ -5,11 +5,22 @@ const GuestBookModal = () => {
   // url 경로
   const urlPath = import.meta.env.VITE_APP_ROUTER_URL
 
+  // 색상 아이템
+  const colorList = [
+    "linear-gradient(180deg, #FFFFFF 0%, #FF6E8A 100%)", // 0: 빨간색
+    "linear-gradient(180deg, #FFFFFF 0%, #FF9E2C 100%)", // 1: 주황색
+    "linear-gradient(180deg, #FFFFFF 0%, #FFC745 100%)", // 2: 노란색
+    "linear-gradient(180deg, #FFFFFF 0%, #27D674 100%)", // 3: 초록색
+    "linear-gradient(180deg, #FFFFFF 0%, #64B1FF 100%)", // 4: 파란색
+    "linear-gradient(180deg, #FFFFFF 0%, #CB9DFF 100%)", // 5: 보라색
+    "linear-gradient(180deg, #FFFFFF 0%, #696969 100%)", // 6: 검정색
+  ]
+
   // 콘텐츠 아이템
   const ContentItem = ({ content }) => {
     return (
       <div className={styles.Item}>
-        <div className={styles.Content}>
+        <div className={styles.Content} style={{ transform: `rotate(${10}deg)`, background: `${colorList[6]}` }}>
           <div className={styles.temp}>
             {content}
           </div>
