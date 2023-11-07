0c84bcef03744db091cf13c4e2d9c5e3f2b6d052
김준형 <wnsgud6232@naver.com>
2023-11-07 20:05:02 +0900

    FEAT: 스타일 적용을 위한 CSS 파일 import

--- frontend/src/components/Modal/GuestBook/DetailGuestBookModal.jsx
+++ frontend/src/components/Modal/GuestBook/DetailGuestBookModal.jsx
@@ -1,7 +1,9 @@
+import styles from './DetailGuestBookModal.module.css'
+
 const DetailGuestBookModal = () => {
   return (
     <>
-      <div>방명록 상세 모달</div>
+      <div className={styles.Container}>방명록 상세 모달</div>
     </>
   )
 }
