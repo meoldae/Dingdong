ab4362962ee5878596808d0559df0bbbbc85ad82
김준형 <wnsgud6232@naver.com>
2023-11-07 15:22:18 +0900

    FEAT: 방명록 작성 모달 상태관리를 위한 아톰 추가

--- frontend/src/atom/GuestBookAtom.jsx
+++ frontend/src/atom/GuestBookAtom.jsx
@@ -4,3 +4,8 @@ export const isGuestBookVisibleAtom = atom({
   key: "isGuestBookAtom",
   default: false,
 })
+
+export const isWriteGuestBookVisibleAtom = atom({
+  key: "isWriteGuestBookAtom",
+  default: false,
+})
