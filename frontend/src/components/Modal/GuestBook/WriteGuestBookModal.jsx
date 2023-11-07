d1009b982df88ecc7268da852d20e5e9364f9462
김준형 <wnsgud6232@naver.com>
2023-11-07 15:40:35 +0900

    FEAT: 스타일 CSS import

--- frontend/src/components/Modal/GuestBook/WriteGuestBookModal.jsx
+++ frontend/src/components/Modal/GuestBook/WriteGuestBookModal.jsx
@@ -1,7 +1,12 @@
+// 스타일
+import styles from './WriteGuestBookModal.module.css'
+
 const WriteGuestBookModal = () => {
   return (
     <>
-      <div>방명록 작성 모달</div>
+      <div className={styles.Container}>
+        방명록 작성모달
+      </div>
     </>
   )
 }
