18b8168e6c898277212a45195e083196c934a544
김준형 <wnsgud6232@naver.com>
2023-11-07 20:25:04 +0900

    FEAT: 방명록 상세를 위한 API함수 생성

--- frontend/src/api/GuestBook.js
+++ frontend/src/api/GuestBook.js
@@ -5,9 +5,14 @@ const fetchWriteGuestBook = async (params, success, fail) => {
     await HttpJson.post(`visitorbook`, params).then(success).catch(fail);
 };
 
-// 방명록 작성
+// 방명록 리스트
 const fetchListGuestBook = async (roomId, success, fail) => {
     await HttpJson.get(`visitorbook/list/${roomId}`).then(success).catch(fail);
 };
 
-export { fetchWriteGuestBook, fetchListGuestBook };
+// 방명록 상세
+const fetchDetailGuestBook = async (guestBookId, success, fail) => {
+    await HttpJson.get(`visitorbook/detail/${guestBookId}`).then(success).catch(fail);
+};
+
+export { fetchWriteGuestBook, fetchListGuestBook, fetchDetailGuestBook };
