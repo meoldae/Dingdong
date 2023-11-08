54eeb4836db54c5bbf7980ffe119b9af047aa164
김준형 <wnsgud6232@naver.com>
2023-11-08 12:56:56 +0900

    FEAT: 닉네임 검색 함수 작성 완료

--- frontend/src/components/Modal/PostOffice/PostOfficeModal.jsx
+++ frontend/src/components/Modal/PostOffice/PostOfficeModal.jsx
@@ -1,9 +1,34 @@
+// 라이브러리
+import { useState } from "react"
+
+// API
+import { fetchSerchNickname } from "../../../api/User"
+
 // 스타일
 import styles from "./PostOfficeModal.module.css"
 
 const PostOfficeModal = () => {
   // url 경로
   const urlPath = import.meta.env.VITE_APP_ROUTER_URL
+
+  // 닉네임 검색 상태관리
+  const [searchText, setSearchText] = useState("")
+
+  // 닉네임 검색 함수
+  const searchNicknameHandler = (event) => {
+    const newText = event.target.value
+    setSearchText(newText)
+
+    fetchSerchNickname(
+      newText,
+      (success) => {
+        console.log(success)
+      },
+      (error) => {
+        console.log("Error at Search Nickname...", error)
+      }
+    )
+  }
 
   return (
     <>
@@ -13,6 +38,12 @@ const PostOfficeModal = () => {
         <div className={styles.SearchContainer}>
           <div className={styles.Search}>
             <img src={`${urlPath}/assets/icons/search.png`} />
+            <input
+              type="text"
+              value={searchText}
+              placeholder="검색"
+              onChange={(e) => searchNicknameHandler(e)}
+            />
           </div>
           <div className={styles.Cancel} style={{ fontFamily: "GmarketSansMedium" }}>취소</div>
         </div>
