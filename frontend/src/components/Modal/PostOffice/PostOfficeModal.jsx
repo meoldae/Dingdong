a093741318ced10c0f2741029acadced0113685c
김준형 <wnsgud6232@naver.com>
2023-11-08 11:18:08 +0900

    FEAT: 검색 아이콘을 추가

--- frontend/src/components/Modal/PostOffice/PostOfficeModal.jsx
+++ frontend/src/components/Modal/PostOffice/PostOfficeModal.jsx
@@ -2,11 +2,20 @@
 import styles from "./PostOfficeModal.module.css"
 
 const PostOfficeModal = () => {
+  // url 경로
+  const urlPath = import.meta.env.VITE_APP_ROUTER_URL
+
   return (
     <>
       <div className={styles.Container}>
-        <div className={styles.Title}>우체국</div>
+        <div className={styles.Title} style={{ fontFamily: "GmarketSansMedium" }}>우체국</div>
         <div className={styles.Line} />
+        <div className={styles.SearchContainer}>
+          <div className={styles.Search}>
+            <img src={`${urlPath}/assets/icons/search.png`} />
+          </div>
+          <div className={styles.Cancel} style={{ fontFamily: "GmarketSansMedium" }}>취소</div>
+        </div>
       </div>
     </>
   )
