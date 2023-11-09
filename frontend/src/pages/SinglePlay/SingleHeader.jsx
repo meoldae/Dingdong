144eb6f2d5b7fa7b60cef110b7b3d4081b468dda
KyongBeom <dragontig98@naver.com>
2023-11-09 09:55:14 +0900

    FIX: 글자 수 넘겼을 때 헤더 밀림 해결

--- frontend/src/pages/SinglePlay/SingleHeader.jsx
+++ frontend/src/pages/SinglePlay/SingleHeader.jsx
@@ -232,9 +232,12 @@ const SingleHeader = ({ checkMyRoom }) => {
                 onClick={() => setIsHamburger(true)}
                 className={styles.HamburgerButton}
               />
+              <div className={styles.Name}>
+
               <RoomNameBtn >
                 딩동 마을
               </RoomNameBtn >
+              </div>
               {/* </div> */}
               <img src={bell} onClick={() => setIsAlarm(true)} />
             </>
