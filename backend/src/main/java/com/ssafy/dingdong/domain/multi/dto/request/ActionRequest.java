222d4ee9395f65a659e3c0b99a88de932d3c73da
Kang Dong Pyo <76652908+97Kzone@users.noreply.github.com>
2023-11-09 14:36:40 +0900

    FIX: 멀티 상호작용 기능 위해 로직 수정

--- backend/src/main/java/com/ssafy/dingdong/domain/multi/dto/request/ActionRequest.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/multi/dto/request/ActionRequest.java
@@ -1,12 +1,12 @@
 package com.ssafy.dingdong.domain.multi.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ActionRequest {
-
     private Long channelId;
     private Long roomId;
-
     private Integer actionId;
 }
