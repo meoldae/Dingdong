68984a81fe7a1789fee45f8be9bcc7d0d9cf1206
김준형 <wnsgud6232@naver.com>
2023-11-13 15:13:07 +0900

    FEAT: floating 버튼 생성 및 로직 작성 스타일 작성

--- frontend/src/components/Multi/MultiPage.jsx
+++ frontend/src/components/Multi/MultiPage.jsx
@@ -3,29 +3,57 @@ import { MultiRender } from "./MultiRender"
 import styles from "./MultiPage.module.css"
 import { Suspense, useRef } from "react"
 import { Physics } from "@react-three/rapier"
+import { useRecoilState } from "recoil"
+import { isFloatingButtonVisibleAtom } from "../../atom/MultiAtom"
 
 export const MultiPage = () => {
+  const urlPath = import.meta.env.VITE_APP_ROUTER_URL
+
   const multiRenderRef = useRef()
+
+  const [isFloatingButtonVisible, setIsFloatingButtonVisible] = useRecoilState(isFloatingButtonVisibleAtom)
 
   const handleButtonClick = (action) => {
     if (multiRenderRef.current?.publishActions) {
       multiRenderRef.current.publishActions(action)
     }
+
+    setIsFloatingButtonVisible(false)
   }
 
   return (
     <div className={styles.container}>
-      <div className={styles.BtnList}>
-        <div className={styles.actionsBtn} onClick={() => handleButtonClick(1)}>
-          기뻐하기
-        </div>
-        <div className={styles.actionsBtn} onClick={() => handleButtonClick(2)}>
-          슬퍼하기
-        </div>
-        <div className={styles.actionsBtn} onClick={() => handleButtonClick(3)}>
-          춤추기
-        </div>
+      <div className={styles.FloatingButton} onClick={() => setIsFloatingButtonVisible(true)}>
+        <img
+          src={`${urlPath}/assets/icons/white_plus.png`}
+          className={`${styles.PlusButton} ${isFloatingButtonVisible ? styles.Rotate : styles.RotateBack}`}
+        />
       </div>
+      {isFloatingButtonVisible && (
+        <>
+        <div className={styles.Overlay} onClick={() => setIsFloatingButtonVisible(false)} />
+        <div className={styles.BtnList}>
+          <div className={styles.actionsBtn} onClick={() => handleButtonClick(1)}>
+            <div className={styles.FloatContent}>기뻐하기</div>
+            <div className={styles.IconButton}>
+              <img src={`${urlPath}/assets/icons/smile.png`} className={styles.FloatIcon} />
+            </div>
+          </div>
+          <div className={styles.actionsBtn} onClick={() => handleButtonClick(2)}>
+            <div className={styles.FloatContent}>슬퍼하기</div>
+            <div className={styles.IconButton}>
+              <img src={`${urlPath}/assets/icons/sad.png`} className={styles.FloatIcon} />
+            </div>
+          </div>
+          <div className={styles.actionsBtn} onClick={() => handleButtonClick(3)}>
+            <div className={styles.FloatContent}>춤추기</div>
+            <div className={styles.IconButton}>
+              <img src={`${urlPath}/assets/icons/dance.png`} className={styles.FloatIcon} />
+            </div>
+          </div>
+        </div>
+        </>
+      )}
       <Canvas shadows camera={{ position: [2, 8, 15], fov: 30, zoom: 0.72 }}>
         <Suspense>
           <Physics>
