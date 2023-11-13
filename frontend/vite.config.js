9a0595caaa844a267416cb87a9e6b033586fa4e7
김장호 <rlawkdgh0406@naver.com>
2023-11-13 09:46:37 +0900

    FIX: 주석 해제

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,15 @@ import react from "@vitejs/plugin-react"
 import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
-  // base: "/feDev/",
+  base: "/feDev/",
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "auto",
       workbox: {
-        importScripts: ["service-worker.js", "firebase-messaging-sw.js"], // 이게 제일 중요
+        // importScripts: ["service-worker.js", "firebase-messaging-sw.js"], // 이게 제일 중요
+         importScripts: ["service-worker.js"],
       },
       devOptions: {
         enabled: true,
