4f86d5392e4b4f344a4ac7cb562a75b3aa5cecee
KyongBeom <dragontig98@naver.com>
2023-11-14 17:32:49 +0900

    FEAT: 사용하지 않는 컴포넌트 정리

--- frontend/src/components/Room/Room.jsx
+++ frontend/src/components/Room/Room.jsx
@@ -1,16 +1,16 @@
-import { useGLTF } from "@react-three/drei";
+// import { useGLTF } from "@react-three/drei";
 
-export const Room = ({ name }) => {
-  const urlPath = import.meta.env.VITE_APP_ROUTER_URL
-  const { scene } = useGLTF(`${urlPath}/assets/models/furnitureItems/${name}.glb`);
-  return (
-    <primitive
-      object={scene}
-      position-y={-0.22}
-      rotation-y={Math.PI / 4}
-    position-z={-0.1}
-    position-x={-0.1}
-    scale={[1,1.0105,1]}
-    />
-  );
-};
+// export const Room = ({ name }) => {
+//   const urlPath = import.meta.env.VITE_APP_ROUTER_URL
+//   const { scene } = useGLTF(`${urlPath}/assets/models/furnitureItems/${name}.glb`);
+//   return (
+//     <primitive
+//       object={scene}
+//       position-y={-0.22}
+//       rotation-y={Math.PI / 4}
+//     position-z={-0.1}
+//     position-x={-0.1}
+//     scale={[1,1.0105,1]}
+//     />
+//   );
+// };
