01dbfc3104a9e71692ae29b155aca400b0a64003
김준형 <wnsgud6232@naver.com>
2023-11-15 17:19:43 +0900

    COMMENT: 주석 작성

--- frontend/src/pages/Postoffice/PostofficeDefaultModal.jsx
+++ frontend/src/pages/Postoffice/PostofficeDefaultModal.jsx
@@ -15,6 +15,7 @@ const PostDefaultModal = (props) => {
   // url 경로
   const urlPath = import.meta.env.VITE_APP_ROUTER_URL
 
+  // 리코일 상태관리
   const setIsFinishPostCard = useSetRecoilState(finishPostofficeCardAtom)
   
   return (
