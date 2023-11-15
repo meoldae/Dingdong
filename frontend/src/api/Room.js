9c1813e53c57764599a511f91d5eaa04b9846d70
김준형 <wnsgud6232@naver.com>
2023-11-15 16:53:42 +0900

    COMMENT: 랜덤방문 주석 작성

--- frontend/src/api/Room.js
+++ frontend/src/api/Room.js
@@ -10,6 +10,7 @@ const updateHeart = async (roomId, success, fail) => {
     await HttpJson.post(`/room/heart/${roomId}`).then(success).catch(fail);
 };
 
+// 랜덤방문
 const getRandomRoom = async (success, fail) => {
     await HttpJson.get(`/room/random`).then(success).catch(fail);
 };
