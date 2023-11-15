db5d917332cb22f0d44ee7330ae256f9437dfe5a
김준형 <wnsgud6232@naver.com>
2023-11-15 10:40:34 +0900

    STYLE: 메뉴바에 버전 표시

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -443,13 +443,17 @@ const Header = ({ checkMyRoom }) => {
                 </div>
               </div> */}
             </div>
-            <div
+            <div className={styles.FooterContainer}>
+              <div className={styles.Version}>
+                v 1.3.0
+              </div>
+              <div
                 className={styles.exitButton}
                 onClick={() => setIsRealSecession(true)}
-                style={{ borderBottom: "1px solid rgba(194, 194, 194, 0.5)" }}
               >
                 회원 탈퇴
               </div>
+            </div>
           </div>
         </>
       )}
