2ea6c0e715881d18a3c88b31c4a2f3a6bafbe047
rlawkdgh0406@naver.com <rlawkdgh0406@naver.com>
2023-11-15 14:00:43 +0900

    FEAT: 멀티 맵 포탈 생성

--- frontend/src/atom/SinglePlayAtom.jsx
+++ frontend/src/atom/SinglePlayAtom.jsx
@@ -1,141 +1,141 @@
-import { atom } from "recoil";
+import { atom } from "recoil"
 
 // 각 건물 도착 시 카메라 조정 및 움직임 제어
 export const ArriveAtom = atom({
   key: "ArriveAtom",
   default: false,
-});
+})
 
 // 입장 여부
 export const ConfirmEnteringRoomAtom = atom({
   key: "ConfirmEnteringHouseAtom",
   default: false,
-});
+})
 export const ConfirmEnteringPostOfficeAtom = atom({
   key: "ConfirmEnteringPostOfficeAtom",
   default: false,
-});
+})
 export const ConfirmEnteringStoreAtom = atom({
   key: "ConfirmEnteringStoreAtom",
   default: false,
-});
+})
 export const ConfirmEnteringOtherRoomAtom = atom({
   key: "ConfirmEnteringOtherRoomAtom",
   default: false,
-});
+})
 export const ConfirmEnteringWorldAtom = atom({
   key: "ConfirmEnteringWorldAtom",
   default: false,
-});
+})
 export const ConfirmEnteringRankAtom = atom({
   key: "ConfirmEnteringRankAtom",
   default: false,
-});
+})
 export const ConfirmEnteringTestAtom = atom({
   key: "ConfirmEnteringTestAtom",
   default: false,
-});
+})
 export const ConfirmEnteringInstaAtom = atom({
   key: "ConfirmEnteringInstaAtom",
   default: false,
-});
+})
 export const ConfirmEnteringTwitterAtom = atom({
   key: "ConfirmEnteringTwitterAtom",
   default: false,
-});
+})
 export const ConfirmEnteringPostBoxAtom = atom({
   key: "ConfirmEnteringPostBoxAtom",
   default: false,
-});
+})
 
 // 포탈
 export const RoomPortalVisibleAtom = atom({
   key: "HousePortalVisibleAtom",
   default: true,
-});
+})
 
 export const RoomPortalPositionAtom = atom({
   key: "RoomPortalPositionAtom",
   default: [0, 0.03, -4.5],
-});
+})
 
 export const PostOfficePortalVisibleAtom = atom({
   key: "PostOfficePortalVisibleAtom",
   default: true,
-});
+})
 export const PostOfficePortalPositionAtom = atom({
   key: "PostOfficePortalPositionAtom",
   default: [16.3, 0.03, 13.5],
-});
+})
 
 export const StorePortalVisibleAtom = atom({
   key: "StorePortalVisibleAtom",
   default: true,
-});
+})
 export const StorePortalPositionAtom = atom({
   key: "StorePortalPositionAtom",
   default: [8.3, 0.03, 19],
-});
+})
 
 export const OtherRoomPortalVisibleAtom = atom({
   key: "OtherRoomPortalVisibleAtom",
   default: true,
-});
+})
 export const OtherRoomPortalPositionAtom = atom({
   key: "OtherRoomPortalPositionAtom",
   default: [-16.2, 0.03, 11],
-});
+})
 
 export const WorldPortalVisibleAtom = atom({
   key: "WorldPortalVisibleAtom",
   default: true,
-});
+})
 export const WorldPortalPositionAtom = atom({
   key: "WorldPortalPositionAtom",
-  default: [0, 0.03, 26],
-});
+  default: [0, 0.03, 20],
+})
 
 export const RankPortalVisibleAtom = atom({
   key: "RankPortalVisibleAtom",
   default: true,
-});
+})
 export const RankPortalPositionAtom = atom({
   key: "RankPortalPositionAtom",
   default: [-16.5, 0.03, 20.8],
-});
+})
 
 export const TestPortalVisibleAtom = atom({
   key: "TestPortalVisibleAtom",
   default: true,
-});
+})
 export const TestPortalPositionAtom = atom({
   key: "TestPortalPositionAtom",
   default: [14.7, 0.03, 19.5],
-});
+})
 
 export const InstaPortalVisibleAtom = atom({
   key: "InstaPortalVisibleAtom",
   default: true,
-});
+})
 export const InstaPortalPositionAtom = atom({
   key: "InstaPortalPositionAtom",
   default: [16.6, 0.03, 19.5],
-});
+})
 
 export const TwitterPortalVisibleAtom = atom({
   key: "TwitterPortalVisibleAtom",
   default: true,
-});
+})
 export const TwitterPortalPositionAtom = atom({
   key: "TwitterPortalPositionAtom",
   default: [18.4, 0.03, 19.5],
-});
+})
 
 export const PostBoxPortalVisibleAtom = atom({
   key: "PostBoxPortalVisibleAtom",
   default: true,
-});
+})
 export const PostBoxPortalPositionAtom = atom({
   key: "PostBoxPortalPositionAtom",
   default: [2.7, 0.03, -1.1],
-});
+})
--- frontend/src/components/Item/MainItems/Portals/DefaultPortal.jsx
+++ frontend/src/components/Item/MainItems/Portals/DefaultPortal.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from "react"
 
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil"
 import {
   DefaultPosition,
   DefaultZoom,
-} from "../../../../atom/DefaultSettingAtom";
-import { CharacterPositionAtom } from "../../../../atom/DefaultSettingAtom";
-import { ArriveAtom } from "../../../../atom/SinglePlayAtom";
+} from "../../../../atom/DefaultSettingAtom"
+import { CharacterPositionAtom } from "../../../../atom/DefaultSettingAtom"
+import { ArriveAtom } from "../../../../atom/SinglePlayAtom"
 
 const DefaultPortal = ({
   setConfirmEnteringLocation,
@@ -14,52 +14,55 @@ const DefaultPortal = ({
   setPortalVisible,
   adjustedAngle,
   adjustedZoom,
+  PortalSize,
 }) => {
   // Canvas 기본 세팅
-  const setDefaultCamPosition = useSetRecoilState(DefaultPosition);
-  const setDefaultZoom = useSetRecoilState(DefaultZoom);
+  const setDefaultCamPosition = useSetRecoilState(DefaultPosition)
+  const setDefaultZoom = useSetRecoilState(DefaultZoom)
 
   // 캐릭터 실시간 위치
-  const characterPosition = useRecoilValue(CharacterPositionAtom);
+  const characterPosition = useRecoilValue(CharacterPositionAtom)
 
   // 도착 후 움직임 제어
-  const setIsArrived = useSetRecoilState(ArriveAtom);
+  const setIsArrived = useSetRecoilState(ArriveAtom)
 
   // 포탈
-  const PortalSize = [1, 1];
+  // const PortalSize = [1, 1]
 
   useEffect(() => {
     // 포탈 영역 내부에 있는지 확인
     const isInsideX =
-      Math.abs(portalPosition[0] - characterPosition[0]) <= PortalSize[0] / 2;
+      Math.abs(portalPosition[0] - characterPosition[0]) <= PortalSize[0] / 2
     const isInsideZ =
-      Math.abs(portalPosition[2] - characterPosition[2]) <= PortalSize[1] / 2;
+      Math.abs(portalPosition[2] - characterPosition[2]) <= PortalSize[1] / 2
 
-    const isInside = isInsideX && isInsideZ;
+    const isInside = isInsideX && isInsideZ
 
     if (isInside) {
       // 카메라 세팅
-      setDefaultCamPosition(adjustedAngle);
-      setDefaultZoom(adjustedZoom);
+      setDefaultCamPosition(adjustedAngle)
+      setDefaultZoom(adjustedZoom)
 
       // 캐릭터 움직임 제어
-      setIsArrived(true);
+      setIsArrived(true)
 
       // 입장
-      setConfirmEnteringLocation(true);
-      setPortalVisible(false);
+      setConfirmEnteringLocation(true)
+      setPortalVisible(false)
     } else {
     }
-  }, [characterPosition]);
+  }, [characterPosition])
 
   return (
     <mesh position={portalPosition}>
       {/* 원하는 위치로 변경 가능 */}
-      <cylinderGeometry args={[0.5, 0.5, 0.2, 20]} />
+      <cylinderGeometry
+        args={[PortalSize[0] / 2, PortalSize[1] / 2, 0.2, 20]}
+      />
       {/* args: [위 반지름, 아래 반지름, 높이, 방사형 세그먼트 수] */}
       <meshStandardMaterial color={"yellow"} />
     </mesh>
-  );
-};
+  )
+}
 
-export default DefaultPortal;
+export default DefaultPortal
