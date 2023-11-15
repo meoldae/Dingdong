921d2b7636f96120c764a31a7c6492920dfaea8f
김준형 <wnsgud6232@naver.com>
2023-11-15 16:54:29 +0900

    COMMENT: 세션관련 주석 작성

--- frontend/src/api/Session.js
+++ frontend/src/api/Session.js
@@ -1,9 +1,11 @@
 import { HttpJson } from "./Http";
 
+// 세션 연결
 const fetchOnSession = async (success, fail) => {
     await HttpJson.get(`member/connect`).then(success).catch(fail);
 };
 
+// 세션 종료
 const fetchOffSession = async (success, fail) => {
   await HttpJson.delete(`member/disconnect`).then(success).catch(fail);
 };
