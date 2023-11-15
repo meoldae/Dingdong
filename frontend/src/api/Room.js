89a56dc52eee56d770c2bd5e3cdde72f78e8a746
김준형 <wnsgud6232@naver.com>
2023-11-15 16:53:24 +0900

    COMMENT: 좋아요 업데이트 주석 작성

--- frontend/src/api/Room.js
+++ frontend/src/api/Room.js
@@ -5,6 +5,7 @@ const isHeartCheck = async (roomId, success, fail) => {
     await HttpJson.get(`/room/heart/${roomId}`).then(success).catch(fail);
 };
 
+// 좋아요 업데이트
 const updateHeart = async (roomId, success, fail) => {
     await HttpJson.post(`/room/heart/${roomId}`).then(success).catch(fail);
 };
