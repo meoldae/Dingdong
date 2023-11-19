fee05669cfb8afd9f4770f751acab5ce66e2bdd7
Junyoung <wnsdud12365@naver.com>
2023-11-19 20:38:25 +0900

    FEAT: 주사위 눈금 전달 추가

--- backend/src/main/java/com/ssafy/dingdong/domain/multi/dto/response/ActionResponse.java
+++ backend/src/main/java/com/ssafy/dingdong/domain/multi/dto/response/ActionResponse.java
@@ -9,4 +9,12 @@ public class ActionResponse {
     private Long channelId;
     private Long roomId;
     private Integer actionId;
+    private Integer diceNumber;
+
+    public ActionResponse(Long channelId, Long roomId, Integer actionId) {
+        this.channelId = channelId;
+        this.roomId = roomId;
+        this.actionId = actionId;
+        this.diceNumber = (int) (Math.random()*6) + 1;
+    }
 }
